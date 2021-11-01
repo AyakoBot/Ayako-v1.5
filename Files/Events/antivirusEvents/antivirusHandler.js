11f927ec8d67bd0707bff653aec8ea26a66a69e5
Lars_und_so <larsundso3@gmail.com>
2021-11-01 23:41:22 +0100

    merged all antivirus Handlers

--- Files/Events/antivirusEvents/antivirusHandler.js
+++ Files/Events/antivirusEvents/antivirusHandler.js
@@ -1,15 +1,15 @@
 module.exports = {
-	async execute(msg, link) {
+	async execute(msg, link, type) {
 		const client = msg.client;
-		const user = client.users.cache.get(msg.authorid);
+		const user = client.users.cache.get(msg.author.id);
 		const guild = msg.guild;
-		
-		if (msg) msg.delete().catch(() => { });
+
+		if (msg) msg.delete().catch(() => {});
 
 		let amountOfTimes = 0;
 		const res = await client.ch.query('SELECT * FROM antiviruslog WHERE userid = $1;', [user.id]);
 		if (res && res.rowCount > 0) amountOfTimes = res.rowCount;
-		client.ch.query('INSERT INTO antiviruslog (guildid, userid, type, dateofwarn) VALUES ($1, $2, $3, $4);', [guild.id, user.id, 'virustotal', Date.now()]);
+		client.ch.query('INSERT INTO antiviruslog (guildid, userid, type, dateofwarn) VALUES ($1, $2, $3, $4);', [guild.id, user.id, type, Date.now()]);
 		amountOfTimes++;
 
 		const settingsRes = await client.ch.query('SELECT * FROM antivirus WHERE guildid = $1 AND active = true;', [guild.id]);
@@ -20,6 +20,6 @@ module.exports = {
 			if (+amountOfTimes >= +r.muteafterwarnsamount && r.mutetof == true) return client.emit('antivirusMuteAdd', msg, link);
 			if (+amountOfTimes >= 2 && r.warntof == true) return client.emit('antivirusOfwarnAdd', msg, link);
 			if (+amountOfTimes >= 1 && r.verbaltof == true) return client.emit('antivirusWarnAdd', msg, link);
-		}
+		} 
 	}
 };
