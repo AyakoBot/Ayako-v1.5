2b5936810a499f266b5a69c21f3a9988b8958ab6
Lars_und_so <larsundso3@gmail.com>
2021-11-03 04:29:05 +0100

    started Check

--- Files/Commands/check.js
+++ Files/Commands/check.js
@@ -8,7 +8,8 @@ module.exports = {
 	aliases: null,
 	type: 'mod',
 	async execute(msg) {
-		const user = msg.args[0] ? await msg.client.users.fetch(msg.args[0].replace(/\D+/g, '')).catch(() => { }) : msg.author;
+		console.log(msg.args);
+		const user = msg.args[0] ? await msg.client.users.fetch(msg.args[0].replace(/\D+/g, '')) : msg.author;
 		const lan = msg.lan;
 		const con = msg.client.constants.commands[this.name];
 
@@ -19,16 +20,46 @@ module.exports = {
 		let warns = 0;
 		let mutes = 0;
 
-		const res = await msg.client.ch.query('SELECT *, ROW_NUMBER() OVER () FROM warns;');
-		if (res && res.rowCount > 0) warns = res.rows.filter(row => row.type == 'Warn').length, mutes = res.rows.filter(row => row.type == 'Mute').length;
+		const res = await msg.client.ch.query('SELECT *, ROW_NUMBER() OVER () FROM warns WHERE userid = $1;', [user.id]);
+		if (res && res.rowCount > 0) {
+			if (res.rows.filter(row => row.type == 'Warn').length > 0) warns = res.rows.filter(row => row.type == 'Warn').length;
+			if (res.rows.filter(row => row.type == 'Mute').length > 0) mutes = res.rows.filter(row => row.type == 'Mute').length;
+		}
+		embed.setDescription(`${msg.client.ch.stp(lan.text, {warns: `${warns}`, mutes: `${mutes}`})}`);
 
-		embed.setDescription(`${msg.client.ch.stp(lan.warns, {warns: warns})}\n${msg.client.ch.stp(lan.mutes, {mutes: mutes})}`);
+		const Warns = new Discord.MessageButton()
+			.setCustomId('warns')
+			.setLabel(lan.warns)
+			.setStyle(warns > 0 ? 'primary' : 'secondary')
+			.setDisabled(mutes == 0);
+		const Mutes = new Discord.MessageButton()
+			.setCustomId('mutes')
+			.setLabel(lan.mutes)
+			.setStyle(mutes > 0 ? 'primary' : 'secondary')
+			.setDisabled(mutes == 0);
 
-		const m = await msg.client.ch.reply(msg, embed);
+		const collector = m.createMessageComponentCollector({ time: 60000 });
+		collector.on('collected', (click) => {
+			Display(click.customId);
+		});
+		collector.on('end', (collected, reason) => {
+			if (reason == 'time') msg.client.ch.collectorEnd(msg);
+		});
+
+		const m = await msg.client.ch.reply(msg, {embeds: [embed], components: msg.client.ch.buttonRower(Warns, Mutes)});
+
 		const ban = await msg.guild.bans.fetch(user.id).catch(() => { });
 		embed.fields = [];
 		if (ban && ban.guild) embed.addField(msg.client.ch.stp(lan.banned, { target: user }), `${msg.language.reason}: \`\`\`${ban.reason}\`\`\``);
-		else embed.addField(msg.client.ch.stp(lan.notbanned, { target: user }), '\u200b');
+		else embed.addField('\u200b', `**${msg.client.ch.stp(lan.notbanned, { target: user })}**`);
 		m.edit({embeds: [embed]});
+
+		async function Display(id) {
+			const embed = new Discord.MessageEmbed()
+				.setAuthor(msg.client.ch.stp(lan.author, { target: user }), con.author.image, con.author.url)
+				
+
+		}
+
 	}
 };
