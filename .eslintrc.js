f7e9ac0756f0b7d81af5a2e7ca10fd6e7e731539
Lars_und_so <larsundso3@gmail.com>
2021-11-05 00:49:17 +0100

    ES6 config

--- .eslintrc.js
+++ .eslintrc.js
@@ -26,3 +26,37 @@ module.exports = {
 		],
 	}
 };
+
+
+
+/*
+module.exports = {
+	'env': {
+		'browser': true,
+		'es2021': true
+	},
+	'extends': 'eslint:recommended',
+	'parserOptions': {
+		'ecmaVersion': 12,
+		'sourceType': 'module'
+	},
+	'rules': {
+		'indent': [
+			'error',
+			'tab'
+		],
+		'linebreak-style': [
+			'error',
+			'windows'
+		],
+		'quotes': [
+			'error',
+			'single'
+		],
+		'semi': [
+			'error',
+			'always'
+		]
+	}
+};
+*/
