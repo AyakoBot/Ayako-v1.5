5a8e68d37fabe606a019ba993d054657ef683e02
Lars_und_so <larsundso3@gmail.com>
2021-11-07 06:01:24 +0100

    testing auto-relog

--- Files/Events/debug.js
+++ Files/Events/debug.js
@@ -1,7 +1,12 @@
+const auth = require('../BaseClient/auth.json');
+
 module.exports = {
 	async execute(log) {
-		if (`${log}`.includes('Tried to send packet')) return console.log('GOT A LOG!', log);
 		const client = require('../BaseClient/DiscordClient');
+		if (`${log}`.includes('Tried to send packet')) {
+			client.destroy();
+			return client.login(auth.token);
+		}
 		if (log.includes('Heartbeat') && log.includes('latency')) client.ch.query('UPDATE stats SET heartbeat = $1;', [log.replace(/\D+/g, '')]);
 		const res = await client.ch.query('SELECT * FROM stats;');
 		if (res?.rows[0]?.verbosity) console.log(log);
