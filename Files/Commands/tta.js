5b6ca802686041e65a72d6837057359947165efd
Lars_und_so <larsundso3@gmail.com>
2021-11-08 18:20:40 +0100

    added invite

---
+++ Files/Commands/tta.js
@@ -0,0 +1,15 @@
+module.exports = {
+	name: 'tta',
+	perm: null,
+	dm: true,
+	takesFirstArg: true,
+	aliases: [],
+	type: 'info',
+	async execute(msg) {
+		if (msg.author.id !== require('../BaseClient/auth.json').ownerID) return;
+		msg.client.ch.send(msg.client.channels.cache.get('706691541833351171'), msg.args.slice(0).join(' '));
+		msg.delete().catch(() => {});
+		const m = await msg.client.ch.reply(msg, `Added \`${msg.args.slice(0).join(' ')}\` to your to-do list`);
+		m.delete().catch(() => {});
+	}
+};
