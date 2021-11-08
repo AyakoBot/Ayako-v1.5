c19f4a03becf3b95b33bf92f142797d9c968773d
Lars_und_so <larsundso3@gmail.com>
2021-11-08 06:01:26 +0100

    added uptime

---
+++ Files/Commands/uptime.js
@@ -0,0 +1,12 @@
+module.exports = {
+	name: 'uptime',
+	perm: null,
+	dm: true,
+	takesFirstArg: false,
+	aliases: [],
+	type: 'info',
+	async execute(msg) {
+		const since = +Date.now() - +msg.client.uptime;
+		msg.client.ch.reply(msg, { content: `${msg.lan.text} <t:${`${since}`.slice(0, -3)}:F> (<t:${`${since}`.slice(0, -3)}:R>)`});
+	}
+};
