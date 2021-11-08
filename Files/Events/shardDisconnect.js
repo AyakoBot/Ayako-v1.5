d57e6b2f149700c47dc6d9fa79ae0d35fe13f839
Lars_und_so <larsundso3@gmail.com>
2021-11-08 17:44:13 +0100

    re-log

--- Files/Events/shardDisconnect.js
+++ Files/Events/shardDisconnect.js
@@ -1,6 +1,10 @@
+const auth = require('../BaseClient/auth.json');
+
 module.exports = {
 	async execute(event, id) {
 		const client = require('../BaseClient/DiscordClient.js');
-		client.ch.logger(`Discord Client Shard with ID ${id} was Disconnected!`, event);
+		client.ch.logger(`Discord Client Shard with ID ${id} was Disconnected!`);
+		client.destroy();
+		client.login(auth.token);
 	}
 };
