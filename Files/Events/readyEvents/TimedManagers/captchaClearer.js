0a7fc0aaaa2e76868c17598d4f63972d640589bb
Lars_und_so <larsundso3@gmail.com>
2021-11-09 02:13:52 +0100

    fix

--- Files/Events/readyEvents/TimedManagers/captchaClearer.js
+++ Files/Events/readyEvents/TimedManagers/captchaClearer.js
@@ -7,9 +7,7 @@ module.exports = {
 		fs.readdir(directory, (err, files) => {
 			if (err) throw err;
 			for (const file of files) {
-				fs.unlink(path.join(directory, file), err => {
-					if (err) throw err;
-				});
+				fs.unlink(path.join(directory, file), () => {});
 			}
 		});
 	}
