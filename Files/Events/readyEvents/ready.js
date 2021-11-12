103077be5da483615a09120efe75f94729a891a7
Lars_und_so <larsundso3@gmail.com>
2021-11-12 11:19:41 +0100

    sort

--- Files/Events/readyEvents/ready.js
+++ Files/Events/readyEvents/ready.js
@@ -6,31 +6,33 @@ module.exports = {
 		const client = require('../../BaseClient/DiscordClient');
 		console.log(`|Logged in as Ayako\n|-> Bot: ${client.user.tag}`);
 		console.log(`Login at ${new Date(Date.now()).toLocaleString()}`);
-		const ch = client.ch;
+		client.guilds.cache.forEach(async guild => client.invites.set(guild.id, await guild.invites.fetch().catch(() => { })));
 		// statcord.autopost();
+		
 		require('./slashcommands.js').execute();
 		require('./muteManager.js').execute();
 		require('./reminder.js').execute();
-		client.guilds.cache.forEach(async guild => client.invites.set(guild.id, await guild.invites.fetch().catch(() => {})));
 		require('./webhooks.js').execute();
 		require('./disboard.js').execute();
 		require('./giveaway.js').execute();
+		require('./separators.js').execute();
+
 		setInterval(() => {
 			require('./colorReminder').execute();
 			//require('./websiteFetcher').execute();
 			if (new Date().getHours() == 0) {
 				client.guilds.cache.forEach(g => {require('../guildEvents/guildCreate/nitro').execute(g);});
 				require('./nitro').execute();
-				ch.query('DELETE FROM toxicitycheck;');
+				client.ch.query('DELETE FROM toxicitycheck;');
 				require('../messageEvents/messageCreate/antispam').resetData();
 				require('../guildEvents/guildMemberAdd/antiraid').resetData();
 			}
 		}, 3600000);
+
 		setInterval(() => require('./TimedManagers/timedManagerSplitter').execute(), 2000);
 		setInterval(() => require('./antiraidBanAdd.js').execute(), 10000);
 		setInterval(() => require('./prunelog.js').execute(), 120000);
 		setInterval(() => require('./presence.js').execute(), 60000);
-		require('./separators.js').execute();
 		setInterval(() => console.log(new Date().toLocaleString()), 600000);
 	}
 };
