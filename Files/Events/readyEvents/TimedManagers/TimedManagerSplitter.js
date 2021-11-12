0957426cc2627c8ce4a8d2fbfe7463e7fda3fccf
Lars_und_so <larsundso3@gmail.com>
2021-11-12 11:11:35 +0100

    removed disboard from timedManagers

--- Files/Events/readyEvents/TimedManagers/TimedManagerSplitter.js
+++ Files/Events/readyEvents/TimedManagers/TimedManagerSplitter.js
@@ -1,15 +1,13 @@
 module.exports = {
 	execute() {
-		require('./reminder').execute();
 		require('./verification').execute();
 		require('./captchaClearer').execute();
 		require('./separatorControl').execute();
 		require('./antivirusClear').execute();
-		return;
-		require('./willis').execute();
-		require('./disboard').execute();
-		require('./voteReminder').execute();
-		require('./mute').execute();
-		require('./stats').execute();
+		//require('./willis').execute();
+		//require('./disboard').execute();
+		//require('./voteReminder').execute();
+		//require('./mute').execute();
+		//require('./stats').execute();
 	}
 };
