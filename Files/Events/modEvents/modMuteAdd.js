2cfb21eb6ef80c9b7c216a71ab531859d78fea04
Lars_und_so <larsundso3@gmail.com>
2021-11-17 14:45:29 +0100

    finished "mute anyways" handler

--- Files/Events/modEvents/modMuteAdd.js
+++ Files/Events/modEvents/modMuteAdd.js
@@ -27,11 +27,32 @@ module.exports = {
 		const exec = await msg.guild.members.fetch(executor.id).catch(() => { });
 		const memberClient = msg.guild.me;
 		if (!member) {
-			if (mexisted) em.fields.pop(), em.addField('\u200b', msg.client.constants.emotes.cross + ' ' + lan.noMember);
-			else em.setDescription(msg.client.constants.emotes.cross + ' ' + lan.noMember);
-			msg.m?.edit({ embeds: [em] }).catch(() => { });
+			if (mexisted) {
+				em.fields.pop();
+				em.addField('\u200b', msg.client.constants.emotes.cross + ' ' + lan.noMember);
+				msg.m?.edit({ embeds: [em] });
+			}
+			else {
+				em.setDescription(msg.client.constants.emotes.cross + ' ' + lan.noMemberHint);
+				const Yes = new Discord.MessageButton()
+					.setCustomId('yes')
+					.setLabel(msg.language.Yes)
+					.setStyle('SUCCESS');
+				const No = new Discord.MessageButton()
+					.setCustomId('no')
+					.setLabel(msg.language.No)
+					.setStyle('DANGER');
+				msg.m?.edit({ embeds: [em], components: msg.client.ch.buttonRower([[Yes, No]]) });
+			}
 			if (mexisted) setTimeout(() => msg.m?.delete().catch(() => { }), 10000);
-			return false;
+			else {
+				const [aborted, answer] = await ask(executor, msg);
+				if (aborted) {
+					em.setDescription(msg.client.constants.emotes.cross + ' ' + lan.noMember);
+					answer ? answer.update({ embeds: [em], components: [] }) : msg.m?.edit({ embeds: [em], components: [] });
+					return false;
+				} else return assingWarn(executor, target, reason, msg, answer, em, language, con, lan, logchannel);
+			}
 		}
 		if (exec?.roles.highest.rawPosition < member?.roles.highest.rawPosition || exec?.roles.highest.rawPosition == member?.roles.highest.rawPosition) {
 			if (mexisted) em.fields.pop(), em.addField('\u200b', msg.client.constants.emotes.cross + ' ' + lan.exeNoPerms);
@@ -94,7 +115,7 @@ module.exports = {
 		const res = await msg.client.ch.query('SELECT * FROM warns WHERE guildid = $1 AND userid = $2;', [msg.guild.id, target.id]);
 		if (res && res.rowCount > 0) warnnr = res.rowCount + 1;
 		else warnnr = 1;
-		if (!msg.source || msg.source !== 'guildMemberAdd')msg.client.ch.query('INSERT INTO warns (guildid, userid, reason, type, dateofwarn, warnedinchannelid, warnedbyuserid, warnedinchannelname, warnedbyusername, msgid) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10);', [msg.guild.id, target.id, reason, 'Mute', Date.now(), msg.channel.id, executor.id, msg.channel.name, executor.username, msg.id]);
+		if (!msg.source || msg.source !== 'guildMemberAdd') msg.client.ch.query('INSERT INTO warns (guildid, userid, reason, type, dateofwarn, warnedinchannelid, warnedbyuserid, warnedinchannelname, warnedbyusername, msgid) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10);', [msg.guild.id, target.id, reason, 'Mute', Date.now(), msg.channel.id, executor.id, msg.channel.name, executor.username, msg.id]);
 		if (mexisted) em.fields.pop(), em.addField('\u200b', msg.client.constants.emotes.tick + ' ' + msg.client.ch.stp(lan.success, { target: target, nr: warnnr }));
 		else em.setDescription(msg.client.constants.emotes.tick + ' ' + msg.client.ch.stp(lan.success, { target: target, nr: warnnr }));
 		await msg.m?.edit({ embeds: [em] }).catch(() => { });
@@ -102,3 +123,54 @@ module.exports = {
 		return true;
 	}
 };
+
+async function ask(executor, msg) {
+	if (!msg.m) return [true];
+	else {
+		const buttonsCollector = msg.m.createMessageComponentCollector({ time: 60000 });
+		const resolved = new Promise((resolve) => {
+			buttonsCollector.on('collect', async (button) => {
+				if (button.user.id !== executor.id) return msg.client.ch.notYours(button, msg.m);
+				if (button.customId == 'yes') {
+					buttonsCollector.stop();
+					resolve([false, button]);
+				} else if (button.customId == 'no') {
+					buttonsCollector.stop();
+					resolve([true, button]);
+				}
+			});
+			buttonsCollector.on('end', (col, reason) => {
+				if (reason == 'time') {
+					msg.client.ch.collectorEnd(msg.m);
+					resolve([true]);
+				}
+			});
+		});
+		return resolved;
+	}
+}
+
+async function assingWarn(executor, target, reason, msg, answer, em, language, con, lan, logchannel) {
+	const dmChannel = await target.createDM().catch(() => { });
+	const DMembed = new Discord.MessageEmbed()
+		.setDescription(`${language.reason}: \`\`\`${reason}\`\`\``)
+		.setColor(con.color)
+		.setTimestamp()
+		.setAuthor(msg.client.ch.stp(lan.dm.author, { guild: msg.guild }), lan.author.image, msg.client.ch.stp(con.author.link, { guild: msg.guild }));
+	msg.client.ch.send(dmChannel, DMembed);
+	const res = await msg.client.ch.query('SELECT * FROM logchannels WHERE guildid = $1;', [msg.guild.id]);
+	if (res && res.rowCount > 0) logchannel = msg.client.channels.cache.get(res.rows[0].guildEvents);
+	const embed = new Discord.MessageEmbed()
+		.setColor(con.color)
+		.setAuthor(lan.author, msg.client.ch.displayAvatarURL(executor), msg.client.constants.standard.invite)
+		.setDescription(msg.client.ch.stp(lan.description, { user: executor, target: target }))
+		.setTimestamp()
+		.setThumbnail(msg.client.ch.displayAvatarURL(target))
+		.addField(language.reason, `\`\`\`${reason}\`\`\``)
+		.setFooter(msg.client.ch.stp(lan.footer, { user: executor, target: target }));
+	if (logchannel) msg.client.ch.send(logchannel, embed);
+	msg.client.ch.query('INSERT INTO warns (guildid, userid, reason, type, dateofwarn, warnedinchannelid, warnedbyuserid, warnedinchannelname, warnedbyusername, msgid) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10);', [msg.guild.id, target.id, reason, 'Mute', Date.now(), msg.channel.id, executor.id, msg.channel.name, executor.username, msg.id]);
+	em.setDescription(msg.client.constants.emotes.tick + ' ' + msg.client.ch.stp(lan.success, { target: target }));
+	await answer.update({ embeds: [em], components: [] }).catch(() => { });
+	return true;
+}
--- Files/Events/modEvents/modMuteRemove.js
+++ Files/Events/modEvents/modMuteRemove.js
@@ -27,11 +27,32 @@ module.exports = {
 		const exec = await msg.guild.members.fetch(executor.id).catch(() => { });
 		const memberClient = msg.guild.me;
 		if (!member) {
-			if (mexisted) em.fields.pop(), em.addField('\u200b', msg.client.constants.emotes.cross + ' ' + lan.noMember);
-			else em.setDescription(msg.client.constants.emotes.cross + ' ' + lan.noMember);
-			msg.m?.edit({ embeds: [em] });
+			if (mexisted) {
+				em.fields.pop();
+				em.addField('\u200b', msg.client.constants.emotes.cross + ' ' + lan.noMember);
+				msg.m?.edit({ embeds: [em] });
+			}
+			else {
+				em.setDescription(msg.client.constants.emotes.cross + ' ' + lan.noMemberHint);
+				const Yes = new Discord.MessageButton()
+					.setCustomId('yes')
+					.setLabel(msg.language.Yes)
+					.setStyle('SUCCESS');
+				const No = new Discord.MessageButton()
+					.setCustomId('no')
+					.setLabel(msg.language.No)
+					.setStyle('DANGER');
+				msg.m?.edit({ embeds: [em], components: msg.client.ch.buttonRower([[Yes, No]]) });
+			}
 			if (mexisted) setTimeout(() => msg.m?.delete().catch(() => { }), 10000);
-			return false;
+			else {
+				const [aborted, answer] = await ask(executor, msg);
+				if (aborted) {
+					em.setDescription(msg.client.constants.emotes.cross + ' ' + lan.noMember);
+					answer ? answer.update({ embeds: [em], components: [] }) : msg.m?.edit({ embeds: [em], components: [] });
+					return false;
+				} else return assingWarn(executor, target, reason, msg, answer, em, language, con, lan, logchannel);
+			}
 		}
 		if (exec?.roles.highest.rawPosition < member?.roles.highest.rawPosition || exec?.roles.highest.rawPosition == member?.roles.highest.rawPosition) {
 			if (mexisted) em.fields.pop(), em.addField('\u200b', msg.client.constants.emotes.cross + ' ' + lan.exeNoPerms);
@@ -99,3 +120,53 @@ module.exports = {
 		return true;
 	}
 };
+
+async function ask(executor, msg) {
+	if (!msg.m) return [true];
+	else {
+		const buttonsCollector = msg.m.createMessageComponentCollector({ time: 60000 });
+		const resolved = new Promise((resolve) => {
+			buttonsCollector.on('collect', async (button) => {
+				if (button.user.id !== executor.id) return msg.client.ch.notYours(button, msg.m);
+				if (button.customId == 'yes') {
+					buttonsCollector.stop();
+					resolve([false, button]);
+				} else if (button.customId == 'no') {
+					buttonsCollector.stop();
+					resolve([true, button]);
+				}
+			});
+			buttonsCollector.on('end', (col, reason) => {
+				if (reason == 'time') {
+					msg.client.ch.collectorEnd(msg.m);
+					resolve([true]);
+				}
+			});
+		});
+		return resolved;
+	}
+}
+
+async function assingWarn(executor, target, reason, msg, answer, em, language, con, lan, logchannel) {
+	const dmChannel = await target.createDM().catch(() => { });
+	const DMembed = new Discord.MessageEmbed()
+		.setDescription(`${language.reason}: \`\`\`${reason}\`\`\``)
+		.setColor(con.color)
+		.setTimestamp()
+		.setAuthor(msg.client.ch.stp(lan.dm.author, { guild: msg.guild }), lan.author.image, msg.client.ch.stp(con.author.link, { guild: msg.guild }));
+	msg.client.ch.send(dmChannel, DMembed);
+	const res = await msg.client.ch.query('SELECT * FROM logchannels WHERE guildid = $1;', [msg.guild.id]);
+	if (res && res.rowCount > 0) logchannel = msg.client.channels.cache.get(res.rows[0].guildEvents);
+	const embed = new Discord.MessageEmbed()
+		.setColor(con.color)
+		.setAuthor(lan.author, msg.client.ch.displayAvatarURL(executor), msg.client.constants.standard.invite)
+		.setDescription(msg.client.ch.stp(lan.description, { user: executor, target: target }))
+		.setTimestamp()
+		.setThumbnail(msg.client.ch.displayAvatarURL(target))
+		.addField(language.reason, `\`\`\`${reason}\`\`\``)
+		.setFooter(msg.client.ch.stp(lan.footer, { user: executor, target: target }));
+	if (logchannel) msg.client.ch.send(logchannel, embed);
+	em.setDescription(msg.client.constants.emotes.tick + ' ' + msg.client.ch.stp(lan.success, { target: target }));
+	await answer.update({ embeds: [em], components: [] }).catch(() => { });
+	return true;
+}
