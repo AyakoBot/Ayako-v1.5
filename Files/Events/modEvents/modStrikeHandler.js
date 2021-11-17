25c310987a3f820ead53fdebc8dd582319c2cea9
Lars_und_so <larsundso3@gmail.com>
2021-11-17 13:25:20 +0100

    better strike

--- Files/Events/modEvents/modStrikeHandler.js
+++ Files/Events/modEvents/modStrikeHandler.js
@@ -13,13 +13,25 @@ module.exports = {
 			let existingWarns = 0;
 			const res = await msg.client.ch.query('SELECT * FROM warns WHERE guildid = $1 AND userid = $2;', [msg.guild.id, target.id]);
 			if (res && res.rowCount > 0) existingWarns = res.rowCount;
-			const neededPunishmentWarnNr = getClosest(existingWarns, msg.res.rows.map(r => +r.warnamount));
-			const r = msg.res.rows.find(r => r.warnamount == neededPunishmentWarnNr);
-			if (r.punishment == 5) msg.client.emit('modBanAdd', executor, target, reason, msg);
-			else if (r.punishment == 4) msg.client.emit('modTempbanAdd', executor, target, reason, msg, r.duration);
-			else if (r.punishment == 3) msg.client.emit('modKickAdd', executor, target, reason, msg);
-			else if (r.punishment == 2) msg.client.emit('modMuteAdd', executor, target, reason, msg);
-			else if (r.punishment == 1) msg.client.emit('modTempmuteAdd', executor, target, reason, msg, r.duration);
+			const r = msg.res.rows.find(r => r.warnamount == existingWarns);
+			if (r && r.punishment == 5) msg.client.emit('modBanAdd', executor, target, reason, msg);
+			else if (r && r.punishment == 4) msg.client.emit('modTempbanAdd', executor, target, reason, msg, r.duration);
+			else if (r && r.punishment == 3) msg.client.emit('modKickAdd', executor, target, reason, msg);
+			else if (r && r.punishment == 2) msg.client.emit('modMuteAdd', executor, target, reason, msg);
+			else if (r && r.punishment == 1) msg.client.emit('modTempmuteAdd', executor, target, reason, msg, r.duration);
+			else {
+				const higher = isHigher(existingWarns, msg.res.rows.map(r => +r.warnamount));
+				if (higher) {
+					const neededPunishmentWarnNr = getClosest(existingWarns, msg.res.rows.map(r => +r.warnamount));
+					const r = msg.res.rows.find(r => r.warnamount == neededPunishmentWarnNr);
+					if (r.punishment == 5) msg.client.emit('modBanAdd', executor, target, reason, msg);
+					else if (r.punishment == 4) msg.client.emit('modTempbanAdd', executor, target, reason, msg, r.duration);
+					else if (r.punishment == 3) msg.client.emit('modKickAdd', executor, target, reason, msg);
+					else if (r.punishment == 2) msg.client.emit('modMuteAdd', executor, target, reason, msg);
+					else if (r.punishment == 1) msg.client.emit('modTempmuteAdd', executor, target, reason, msg, r.duration);
+					else msg.client.emit('modWarnAdd', executor, target, reason, msg);
+				} else msg.client.emit('modWarnAdd', executor, target, reason, msg);
+			}
 		}
 		return true;
 	}
@@ -37,4 +49,11 @@ function getClosest(num, arr) {
 		}
 	}
 	return curr;
+}
+
+function isHigher(num, arr) {
+	for (var i = 0; i < arr.length; i++) {
+		if (num <= arr[i]) return false;
+	}
+	return true;
 }
