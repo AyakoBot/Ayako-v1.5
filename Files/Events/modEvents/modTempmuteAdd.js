4719f5cc291fa6c9431a3d54f5e5bf744c4a907e
Lars_und_so <larsundso3@gmail.com>
2021-11-19 10:03:36 +0100

    vc Mute added

--- Files/Events/modEvents/modTempmuteAdd.js
+++ Files/Events/modEvents/modTempmuteAdd.js
@@ -90,6 +90,7 @@ module.exports = {
 					($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11);`, 
 					[msg.guild.id, target.id, reason, 'Mute', now + +duration, false, now, msg.channel.id, executor.id, msg.channel.name, msg.author.username]);
 				}
+				if (member && member.voice.channelId !== null && !member.voice.serverMute) member.voice.setMute(true, lan.vcReason).catch(() => {});
 				const dmChannel = await target.createDM().catch(() => {});
 				const DMembed = new Discord.MessageEmbed()
 					.setDescription(`${language.reason}: \n${reason}`)
