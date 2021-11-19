0a541abeb9a633b05fbb930a3a8f3e1f7a17920b
Lars_und_so <larsundso3@gmail.com>
2021-11-19 10:03:21 +0100

    await :/

--- Files/Events/voiceStateEvents/mute.js
+++ Files/Events/voiceStateEvents/mute.js
@@ -7,12 +7,12 @@ module.exports = {
 			const client = newState.guild.client;
 			const mute = client.mutes.get(`${guild.id}-${user.id}`);
 			if (mute) {
-				const language = client.ch.languageSelector(guild.id);
+				const language = await client.ch.languageSelector(guild.id);
 				const lan = language.commands.tempmute;
 				newState.setMute(true, lan.vcReason).catch(() => { });
 			} else if (!newState.serverMute) {
 				const res = await client.ch.query('SELECT * FROM warns WHERE userid = $1 AND guildid = $2 AND closed = false;', [user.id, guild.id]);
-				const language = client.ch.languageSelector(guild.id);
+				const language = await client.ch.languageSelector(guild.id);
 				if (res && res.rowCount > 0 && newState.serverMute) {
 					const lan = language.commands.tempmute;
 					newState.setMute(true, lan.vcReason).catch(() => { });
