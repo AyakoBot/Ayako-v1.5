0708858e06079b532a7a310839bd95af77206a2f
Lars_und_so <larsundso3@gmail.com>
2021-11-19 20:58:02 +0100

    re-did antiraid

--- Files/Events/guildEvents/guildMemberAdd/antiraid.js
+++ Files/Events/guildEvents/guildMemberAdd/antiraid.js
@@ -1,38 +1,73 @@
 const client = require('../../../BaseClient/DiscordClient');
 const Discord = require('discord.js');
+
 const antiraidSettings = {
-	maxInterval: 15000,
-	banThreshold: 10
+	time: 15000,
+	threshold: 15,
+	similarIDThreshold: 5
 };
-const cache = new Discord.Collection();
+
+client.antiraidCache = new Discord.Collection();
 
 module.exports = {
-	async execute(member, user) {
-		cache.map(o => o).forEach(o => {
-			o.joined.forEach(join => {
-				if (Date.now() - join.join > antiraidSettings.maxInterval) cache.get(o.guild, {guild: o.guild, joined: cache.get(o.guild.id).joined.splice(cache.get(o.guild.id).joined.indexOf(join.id), 1)});	
-			});
-			if (o.joined.length == 0) cache.delete(o.guild);
-		});
-		if (user.id == member.client.user.id) return;
-		if (user.bot) return;
-		if (user.flags && user.flags.bitfield !== 0) return;
-		const res = await member.client.ch.query('SELECT * FROM antiraidsettings WHERE guildid = $1 AND active = $2;', [member.guild.id, true]);
-		if (!res || res.rowCount == 0) return;
-		if (cache.get(member.guild.id)) cache.get(member.guild.id).joined.push({id: user.id, join: member.joinedTimestamp, guild: member.guild.id});
-		else {
-			cache.set(member.guild.id, {
-				guild: member.guild,
-				joined: [{id: user.id, join: member.joinedTimestamp, guild: member.guild.id}]
-			});
-		}
-		const joined = cache.get(member.guild.id).joined.filter(member => Date.now() - member.join < antiraidSettings.maxInterval);
-		console.log(2, joined,cache);
-		if (joined.length >= antiraidSettings.banThreshold) await require('../../readyEvents/antiraidBanAdd').add(joined);
-		return;
+	async execute(member) {
+		if (!member || !member.guild) return;
+		//const res = await member.client.ch.query('SELECT * FROM antiraidsettings WHERE guildid = $1 AND active = true;', [member.guild.id]);
+		//if (!res || res.rowCount == 0) return;
+		const caches = this.check(member);
+		this.addMember(member);
+		if (caches) this.punish(caches);
 	},
-	resetData() {
-		client.AntiRaidCache = new Discord.Collection();
+	addMember(member) {
+		const guild = client.antiraidCache.get(member.guild.id);
+		const memberObject = {
+			id: member.user.id,
+			joinedAt: Date.now(),
+			joinCount: 1,
+			idIdent: member.user.id.slice(0, 3),
+			guild: member.guild.id,
+			timeout: setTimeout(() => 
+				guild.length > 1 ?
+					guild.delete(member.user.id) : 
+					guild.splice(guild.findIndex(m => m.id = member.user.id), 1), 
+			antiraidSettings.time)
+		};
+		
+		if (guild) {
+			const exists = guild.findIndex(m => m.id == member.user.id) == -1 ? false : true;
+			if (exists) {
+				const existingMember = guild[guild.findIndex(m => m.id == member.user.id)];
+
+				clearTimeout(existingMember.timeout);
+				existingMember.timeout = setTimeout(() =>
+					guild.length > 1 ?
+						guild.delete(member.user.id) :
+						guild.splice(guild.findIndex(m => m.id = member.user.id), 1),
+				antiraidSettings.time);
+
+				existingMember.joinCount = existingMember.joinCount + 1;
+
+			} else guild.push(memberObject);
+		} else client.antiraidCache.set(member.guild.id, [memberObject]);
+	},
+	check(member) {
+		let caches = null;
+		const guild = client.antiraidCache.get(member.guild.id);
+		if (guild.length >= antiraidSettings.threshold) caches = guild;
+		else {
+			const findIndexEntries = guild[guild.findIndex(m => m.id == member.user.id)];
+			if (findIndexEntries.joinCount >= antiraidSettings.threshold) caches = [findIndexEntries];
+			else {
+				const filterEntries = guild.filter(m => m.idIdent == member.user.id.slice(0, 3));
+				if (filterEntries.length >= antiraidSettings.similarIDThreshold) caches = filterEntries;
+			}
+		}
+
+		if (caches?.length > 0) return caches;
+		else return;
+	},
+	punish(member, caches) {
+		console.log('UP FOR BAN', caches);
 	}
 };
 
