ff6b3fd67f5cd74875e19a792a0f32a6e5b88ac6
Lars_und_so <larsundso3@gmail.com>
2021-11-22 16:31:48 +0100

    add roles, remove roles and confirmation

--- Files/Commands/settings/autopunish.js
+++ Files/Commands/settings/autopunish.js
@@ -32,6 +32,11 @@ module.exports = {
 			},
 			{
 				name: '\u200b',
+				value: msg.lan.punishmentSettings,
+				inline: false
+			},
+			{
+				name: '\u200b',
 				value: msg.client.ch.stp(msg.lan.warnamount, { warns: r.warnamount ? r.warnamount : '--'}),
 				inline: false
 			},
@@ -44,7 +49,37 @@ module.exports = {
 				name: msg.lan.duration,
 				value: moment.duration(+r.duration).format(`Y [${msg.language.time.years}], M [${msg.language.time.months}], W [${msg.language.time.weeks}], d [${msg.language.time.days}], h [${msg.language.time.hours}], m [${msg.language.time.minutes}], s [${msg.language.time.seconds}]`, { trim: 'all' }) ? moment.duration(+r.duration).format(`Y [${msg.language.time.years}], M [${msg.language.time.months}], W [${msg.language.time.weeks}], d [${msg.language.time.days}], h [${msg.language.time.hours}], m [${msg.language.time.minutes}], s [${msg.language.time.seconds}]`, { trim: 'all' }) : msg.language.none,
 				inline: false
-			}
+			},
+			{
+				name: '\u200b',
+				value: msg.lan.roleSettings,
+				inline: false
+			},
+			{
+				name: msg.lan.addroles,
+				value: `${r.addroles && r.addroles.length > 0 ? r.addroles.map(id => ` <@&${id}>`) : msg.language.none}`,
+				inline: false
+			},
+			{
+				name: msg.lan.removeroles,
+				value: `${r.removeroles && r.removeroles.length > 0 ? r.removeroles.map(id => ` <@&${id}>`) : msg.language.none}`,
+				inline: false
+			},
+			{
+				name: '\u200b',
+				value: msg.lan.confirmationSettings,
+				inline: false
+			},
+			{
+				name: msg.lan.confirmationreq,
+				value: r.confirmationreq ? msg.client.constants.emotes.tick + ' ' + msg.language.enabled : msg.client.constants.emotes.cross + ' ' + msg.language.disabled,
+				inline: true
+			},
+			{
+				name: msg.lan.punishmentawaittime,
+				value: moment.duration(+r.punishmentawaittime).format(`Y [${msg.language.time.years}], M [${msg.language.time.months}], W [${msg.language.time.weeks}], d [${msg.language.time.days}], h [${msg.language.time.hours}], m [${msg.language.time.minutes}], s [${msg.language.time.seconds}]`, { trim: 'all' }) ? moment.duration(+r.punishmentawaittime).format(`Y [${msg.language.time.years}], M [${msg.language.time.months}], W [${msg.language.time.weeks}], d [${msg.language.time.days}], h [${msg.language.time.hours}], m [${msg.language.time.minutes}], s [${msg.language.time.seconds}]`, { trim: 'all' }) : msg.language.none,
+				inline: true
+			},
 		);
 		return embed;
 	},
@@ -65,6 +100,23 @@ module.exports = {
 			.setCustomId(msg.lan.edit.duration.name)
 			.setLabel(msg.lan.edit.duration.name)
 			.setStyle('PRIMARY');
-		return [[active], [warnamount, punishment, duration]];
+		const addroles = new Discord.MessageButton()
+			.setCustomId(msg.lan.edit.addroles.name)
+			.setLabel(msg.lan.addroles)
+			.setStyle('PRIMARY');
+		const removeroles = new Discord.MessageButton()
+			.setCustomId(msg.lan.edit.removeroles.name)
+			.setLabel(msg.lan.removeroles)
+			.setStyle('PRIMARY');
+		const confirmationreq = new Discord.MessageButton()
+			.setCustomId(msg.lan.edit.confirmationreq.name)
+			.setLabel(msg.lan.confirmationreq)
+			.setStyle(r.confirmationreq ? 'SUCCESS' : 'SECONDARY');
+		const punishmentawaittime = new Discord.MessageButton()
+			.setCustomId(msg.lan.edit.punishmentawaittime.name)
+			.setLabel(msg.lan.punishmentawaittime)
+			.setStyle(r.isvarying ? 'SUCCESS' : 'SECONDARY');
+
+		return [[active], [warnamount, punishment, duration], [addroles, removeroles], [confirmationreq, punishmentawaittime]];	
 	}
 };
--- Files/Events/modEvents/modStrikeHandler.js
+++ Files/Events/modEvents/modStrikeHandler.js
@@ -14,26 +14,79 @@ module.exports = {
 			const res = await msg.client.ch.query('SELECT * FROM warns WHERE guildid = $1 AND userid = $2;', [msg.guild.id, target.id]);
 			if (res && res.rowCount > 0) existingWarns = res.rowCount;
 			const r = msg.res.rows.find(r => r.warnamount == existingWarns);
-			if (r && r.punishment == 5) msg.client.emit('modBanAdd', executor, target, reason, msg);
-			else if (r && r.punishment == 4) msg.client.emit('modTempbanAdd', executor, target, reason, msg, r.duration ? r.duration : 3600000);
-			else if (r && r.punishment == 3) msg.client.emit('modKickAdd', executor, target, reason, msg);
-			else if (r && r.punishment == 2) msg.client.emit('modMuteAdd', executor, target, reason, msg);
-			else if (r && r.punishment == 1) msg.client.emit('modTempmuteAdd', executor, target, reason, msg, r.duration ? r.duration : 3600000);
+			if (r.punishment == 6) doPunishment('modWarnAdd', executor, target, reason, msg);
+			else if (r.punishment == 5) doPunishment('modBanAdd', executor, target, reason, msg);
+			else if (r && r.punishment == 4) doPunishment('modTempbanAdd', executor, target, reason, msg);
+			else if (r && r.punishment == 3) doPunishment('modKickAdd', executor, target, reason, msg);
+			else if (r && r.punishment == 2) doPunishment('modMuteAdd', executor, target, reason, msg);
+			else if (r && r.punishment == 1) doPunishment('modTempmuteAdd', executor, target, reason, msg);
 			else {
 				const higher = isHigher(existingWarns, msg.res.rows.map(r => +r.warnamount));
 				if (higher) {
 					const neededPunishmentWarnNr = getClosest(existingWarns, msg.res.rows.map(r => +r.warnamount));
 					const r = msg.res.rows.find(r => r.warnamount == neededPunishmentWarnNr);
-					if (r.punishment == 5) msg.client.emit('modBanAdd', executor, target, reason, msg);
-					else if (r.punishment == 4) msg.client.emit('modTempbanAdd', executor, target, reason, msg, r.duration ? r.duration : 3600000);
-					else if (r.punishment == 3) msg.client.emit('modKickAdd', executor, target, reason, msg);
-					else if (r.punishment == 2) msg.client.emit('modMuteAdd', executor, target, reason, msg);
-					else if (r.punishment == 1) msg.client.emit('modTempmuteAdd', executor, target, reason, msg, r.duration ? r.duration : 3600000);
-					else msg.client.emit('modWarnAdd', executor, target, reason, msg);
-				} else msg.client.emit('modWarnAdd', executor, target, reason, msg);
+					if (r.punishment == 6) doPunishment('modWarnAdd', executor, target, reason, msg);
+					else if (r.punishment == 5) doPunishment('modBanAdd', executor, target, reason, msg);
+					else if (r.punishment == 4) doPunishment('modTempbanAdd', executor, target, reason, msg);
+					else if (r.punishment == 3) doPunishment('modKickAdd', executor, target, reason, msg);
+					else if (r.punishment == 2) doPunishment('modMuteAdd', executor, target, reason, msg);
+					else if (r.punishment == 1) doPunishment('modTempmuteAdd', executor, target, reason, msg);
+					else doPunishment('modWarnAdd', executor, target, reason, msg);
+				} else doPunishment('modWarnAdd', executor, target, reason, msg);
 			}
 		}
 		return true;
+
+		async function doPunishment(punishment, executor, target, reason, msg, r) {
+			doRoles(r);
+			if (punishment == 'modWarnAdd') msg.client.emit(punishment, executor, target, reason, msg, r.duration ? r.duration : 3600000);
+			else {
+				const embed = new Discord.MessageEmbed()
+					.setAuthor(lan.confirmEmbed.author)
+					.setDescription(msg.client.ch.stp(lan.confirmEmbed.description, { user: target, punishment: msg.language.autopunish[r.punishment] }))
+					.setColor(con.confirmEmbed.color);
+				const yes = new Discord.MessageButton()
+					.setLabel(msg.language.Yes)
+					.setStyle('primary')
+					.setCustomId('yes');
+				const no = new Discord.MessageButton()
+					.setLabel(msg.language.No)
+					.setStyle('danger')
+					.setCustomId('no');
+				msg.m = await msg.client.ch.reply(msg, { embeds: [embed], components: msg.client.ch.buttonRower([yes, no])});
+				const agreed = await new Promise((resolve,) => {
+					const buttonsCollector = msg.m.createMessageComponentCollector({time: 60000});
+					buttonsCollector.on((button) => {
+						if (button.user.id == msg.author.id) {
+							if (button.customId == 'yes') {
+								resolve(true);
+							} else if (button.customId == 'no') {
+								buttonsCollector.stop();
+								resolve(false);
+							}
+						} else msg.client.ch.notYours(button, msg);
+					});
+					buttonsCollector.on('end', (collected, reason) => {
+						if (reason == 'time') resolve(false);
+					});
+				});
+				if (agreed) msg.client.emit(punishment, executor, target, reason, msg, r.duration ? r.duration : 3600000);
+				else msg.client.emit('modWarnAdd', executor, target, reason, msg);
+			} 
+		}
+		async function doRoles(r) {
+			const member = msg.guild.members.cache.get(member.id);
+			if (member) {
+				if (r.addroles && r.addroles.length > 0) {
+					const roles = checkRoles(r.addroles, msg.guild);
+					await member.roles.add(roles, msg.language.autotypes.autopunish);
+				}
+				if (r.removeroles && r.removeroles.length > 0) {
+					const roles = checkRoles(r.removeroles, msg.guild);
+					await member.roles.remove(roles, msg.language.autotypes.autopunish);
+				}
+			}
+		}
 	}
 };
 
@@ -56,4 +109,12 @@ function isHigher(num, arr) {
 		if (num <= arr[i]) return false;
 	}
 	return true;
+}
+
+function checkRoles(roles, guild) {
+	roles.forEach((r, i) => {
+		const role = guild.roles.cache.get(r);
+		if (!role || !role.id) roles.splice(i, 1);
+	});
+	return roles;
 }
