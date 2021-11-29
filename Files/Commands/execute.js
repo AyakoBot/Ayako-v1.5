b9a502dd4c0bb6f77dbc09360fe9deb6d0eeed71
Lars_und_so <larsundso3@gmail.com>
2021-11-29 23:44:06 +0100

    better exe

--- Files/Commands/execute.js
+++ Files/Commands/execute.js
@@ -1,11 +1,17 @@
+/* eslint-disable no-unused-vars */
+const axios = require('axios');
+const fs = require('fs');
+const superagent = require('superagent');
+const Discord = require('discord.js');
+
 module.exports = {
 	name: 'execute',
 	aliases: ['e'],
 	perm: 268435456n,
 	dm: true,
 	takesFirstArg: false,
-	// eslint-disable-next-line no-unused-vars
-	execute(msg) {
+	async execute(msg) {
 
 	}
-};
+}; 
+
