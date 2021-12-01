30dc3b5c8eb09c640cf2f8730fcf6140514a12c1
Lars_und_so <larsundso3@gmail.com>
2021-12-01 22:31:16 +0100

    airbnb'ify

--- Files/BaseClient/APILog.js
+++ Files/BaseClient/APILog.js
@@ -5,17 +5,20 @@ const reqOutput = fs.createWriteStream('./Files/Downloads/Logs/Req.log');
 const reqErrorOutput = fs.createWriteStream('./Files/Downloads/Logs/reqErr.log');
 
 const reqLogger = new Console({
-	stdout: reqOutput, stderr: reqErrorOutput,
-	ignoreErrors: true, colorMode: false
+  stdout: reqOutput,
+  stderr: reqErrorOutput,
+  ignoreErrors: true,
+  colorMode: false,
 });
-
 
 const resOutput = fs.createWriteStream('./Files/Downloads/Logs/Res.log');
 const resErrorOutput = fs.createWriteStream('./Files/Downloads/Logs/resErr.log');
 
 const resLogger = new Console({
-	stdout: resOutput, stderr: resErrorOutput,
-	ignoreErrors: true, colorMode: false
+  stdout: resOutput,
+  stderr: resErrorOutput,
+  ignoreErrors: true,
+  colorMode: false,
 });
 
-module.exports = {reqLogger, resLogger};
+module.exports = { reqLogger, resLogger };
--- Files/BaseClient/DBL.js
+++ Files/BaseClient/DBL.js
@@ -1,18 +1,21 @@
 const AutoPoster = require('topgg-autoposter');
 const SDK = require('@top-gg/sdk');
-const auth = require('./auth.json');
-const client = require('./DiscordClient.js');
-const ch = require('./ClientHelper');
 const express = require('express');
+const auth = require('./auth.json');
+const client = require('./DiscordClient');
+const ch = require('./ClientHelper');
 
 const AP = AutoPoster(auth.topggToken, client);
 const app = express();
 const webhook = new SDK.Webhook(auth.webhookPW);
 
-app.post('/dblwebhook', webhook.listener((vote) => {
-	ch.send(client.channels.cache.get('834770912724647966'), `${vote.user} | ${vote.isWeekend}`);
-	console.log(vote);
-}));
+app.post(
+  '/dblwebhook',
+  webhook.listener((vote) => {
+    ch.send(client.channels.cache.get('834770912724647966'), `${vote.user} | ${vote.isWeekend}`);
+    console.log(vote);
+  }),
+);
 app.listen(80);
 
 module.exports = { AP };
