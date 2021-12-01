2044c0e313a41a2a1e93c7d1d7928a38eeac0c81
Lars_und_so <larsundso3@gmail.com>
2021-12-01 21:58:13 +0100

    airbnb'ify

--- Files/BaseClient/DiscordClient.js
+++ Files/BaseClient/DiscordClient.js
@@ -1,71 +1,71 @@
+/* eslint-disable global-require,import/no-dynamic-require */
 const Discord = require('discord.js');
-const auth = require('./auth.json');
 const fs = require('fs');
+const auth = require('./auth.json');
 const Eris = require('./ErisClient');
 
-//Create Discord Client
-const client = new Discord.Client({ 
-	shards:'auto', 
-	partials: ['MESSAGE', 'REACTION', 'CHANNEL', 'USER', 'GUILD_MEMBER'],
-	intents: new Discord.Intents(14335),
-	allowedMentions: {
-		parse: ['users', 'roles'],
-		repliedUser: false
-	},
-	failIfNotExists: false
+// Create Discord Client
+const client = new Discord.Client({
+  shards: 'auto',
+  partials: ['MESSAGE', 'REACTION', 'CHANNEL', 'USER', 'GUILD_MEMBER'],
+  intents: new Discord.Intents(14335),
+  allowedMentions: {
+    parse: ['users', 'roles'],
+    repliedUser: false,
+  },
+  failIfNotExists: false,
 });
 
-//Create Command Collection and gather all Command Files
+// Create Command Collection and gather all Command Files
 client.commands = new Discord.Collection();
-const commandFiles = fs.readdirSync('./Files/Commands').filter(file => file.endsWith('.js'));
-for (const file of commandFiles) {
-	const command = require(`../Commands/${file}`);
-	client.commands.set(command.name, command);
-}
+const commandFiles = fs.readdirSync('./Files/Commands').filter((file) => file.endsWith('.js'));
+commandFiles.forEach((file) => {
+  const command = require(`../Commands/${file}`);
+  client.commands.set(command.name, command);
+});
 
-//Create Language Collection and gather all Language Files
+// Create Language Collection and gather all Language Files
 client.languages = new Discord.Collection();
-const languageFiles = fs.readdirSync('./Files/Languages').filter(file => file.endsWith('.json'));
-for (const file of languageFiles) {
-	const language = require(`../Languages/${file}`);
-	const name = file.replace(/.json/g, '');
-	client.languages.set(name, language);
-}
-//Create Event Collection and gather all Event Files
+const languageFiles = fs.readdirSync('./Files/Languages').filter((file) => file.endsWith('.json'));
+languageFiles.forEach((file) => {
+  const language = require(`../Languages/${file}`);
+  const name = file.replace(/.json/g, '');
+  client.languages.set(name, language);
+});
+
+// Create Event Collection and gather all Event Files
 client.events = new Discord.Collection();
 const eventsDir = fs.readdirSync('./Files/Events');
 const reg = new RegExp('.js', 'g');
-for (const folder of eventsDir) {
-	if (folder.endsWith('.js')) {
-		const event = require(`../Events/${folder}`);
-		event.path = `../Events/${folder}`;
-		client.events.set(folder.replace(reg, ''), event);
-	} else {
-		const key = folder.replace(/Events/g, '');
-		const eventFiles = fs.readdirSync(`./Files/Events/${folder}`);
-		for (const file of eventFiles) {
-			if (file.endsWith('.js') && file.startsWith(key)) {
-				const event = require(`../Events/${folder}/${file}`);
-				event.path = `../Events/${folder}/${file}`;
-				client.events.set(file.replace(reg, ''), event);
-			} else {
-				if (file.startsWith(key) && !file.endsWith('.js')) {
-					for (const eventFolderFile of fs.readdirSync(`./Files/Events/${folder}/${file}`)) {
-						if (`${eventFolderFile}`.endsWith('.js') && `${eventFolderFile}`.startsWith(key)) {
-							const event = require(`../Events/${folder}/${file}/${eventFolderFile}`);
-							event.path = `../Events/${folder}/${file}/${eventFolderFile}`;
-							client.events.set(eventFolderFile.replace(reg, ''), event);
-						} 
-					}
-				}
-			}
-		}
-	}
-}
+eventsDir.forEach((folder) => {
+  if (folder.endsWith('.js')) {
+    const event = require(`../Events/${folder}`);
+    event.path = `../Events/${folder}`;
+    client.events.set(folder.replace(reg, ''), event);
+  } else {
+    const key = folder.replace(/Events/g, '');
+    const eventFiles = fs.readdirSync(`./Files/Events/${folder}`);
+    eventFiles.forEach((file) => {
+      if (file.endsWith('.js') && file.startsWith(key)) {
+        const event = require(`../Events/${folder}/${file}`);
+        event.path = `../Events/${folder}/${file}`;
+        client.events.set(file.replace(reg, ''), event);
+      } else if (file.startsWith(key) && !file.endsWith('.js')) {
+        fs.readdirSync(`./Files/Events/${folder}/${file}`).forEach((eventFolderFile) => {
+          if (`${eventFolderFile}`.endsWith('.js') && `${eventFolderFile}`.startsWith(key)) {
+            const event = require(`../Events/${folder}/${file}/${eventFolderFile}`);
+            event.path = `../Events/${folder}/${file}/${eventFolderFile}`;
+            client.events.set(eventFolderFile.replace(reg, ''), event);
+          }
+        });
+      }
+    });
+  }
+});
 
-//Connect to Discord
+// Connect to Discord
 client.login(auth.token).then(() => {
-	console.log('| Discord Client connected at '+ new Date().toUTCString());
+  console.log(`| Discord Client connected at ${new Date().toUTCString()}`);
 });
 
 client.invites = new Map();
@@ -73,6 +73,7 @@ client.mutes = new Discord.Collection();
 client.bans = new Discord.Collection();
 client.channelWebhooks = new Map();
 client.constants = require('../Constants.json');
+
 client.setMaxListeners(63);
 client.antiraidCache = new Discord.Collection();
 
