9c6468d6fac09ebbe5a073b56d8e3c7739161ff1
Lars_und_so <larsundso3@gmail.com>
2021-12-07 15:11:41 +0100

    made compatible with new Settings manager

--- Files/Commands/settings/editors/boolean.js
+++ Files/Commands/settings/editors/boolean.js
@@ -1,8 +1,9 @@
+/* eslint-disable no-param-reassign */
 module.exports = {
-	key: ['boolean'],
-	async execute(msg, i, embed, values, answer, AddRemoveEditView, fail, srmEditing, comesFromSRM) {
-		if (!msg.rows[msg.assigner]) values[msg.assigner] = true;
-		else values[msg.assigner] = false;
-		return ['repeater', msg, i + 1, embed, values, answer, AddRemoveEditView, fail, srmEditing, comesFromSRM];
-	}
+  key: ['boolean'],
+  requiresInteraction: false,
+  execute(msg, required, values) {
+    if (!msg.rows[required.assigner]) values[required.assigner] = true;
+    else values[msg.assigner] = false;
+  },
 };
