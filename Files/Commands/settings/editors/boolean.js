23c9630c779d86e151968f94b35307df9de15a5c
Lars_und_so <larsundso3@gmail.com>
2021-12-13 17:33:29 +0100

    finished

--- Files/Commands/settings/editors/boolean.js
+++ Files/Commands/settings/editors/boolean.js
@@ -2,8 +2,8 @@
 module.exports = {
   key: ['boolean'],
   requiresInteraction: false,
-  execute(msg, required, values) {
-    if (!msg.rows[required.assigner]) values[required.assigner] = true;
-    else values[msg.assigner] = false;
+  execute(msg, required, values, row) {
+    if (!row[required.assinger]) values[required.assinger] = true;
+    else values[required.assinger] = false;
   },
 };
