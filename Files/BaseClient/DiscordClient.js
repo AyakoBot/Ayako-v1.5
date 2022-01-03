ae3eec3fdb46d5af359c382e0fd6b87a26cb7fb5
Lars_und_so <larsundso3@gmail.com>
2022-01-03 23:48:25 +0100

    re-arranged

--- Files/BaseClient/DiscordClient.js
+++ Files/BaseClient/DiscordClient.js
@@ -64,10 +64,11 @@ const getSettingsEditors = () => {
 
 const getCommands = () => {
   const commands = new Discord.Collection();
-
   const commandFiles = fs.readdirSync('./Files/Commands').filter((file) => file.endsWith('.js'));
+
   commandFiles.forEach((file) => {
     const command = require(`../Commands/${file}`);
+
     commands.set(command.name, command);
   });
 
@@ -83,6 +84,7 @@ const getLanguages = () => {
   languageFiles.forEach((file) => {
     const language = require(`../Languages/${file}`);
     const name = file.replace(/.json/g, '');
+
     languages.set(name, language);
   });
 
@@ -93,24 +95,29 @@ const getEvents = () => {
   const events = new Discord.Collection();
   const eventsDir = fs.readdirSync('./Files/Events');
   const reg = new RegExp('.js', 'g');
+
   eventsDir.forEach((folder) => {
     if (folder.endsWith('.js')) {
       const event = require(`../Events/${folder}`);
       event.path = `../Events/${folder}`;
+
       events.set(folder.replace(reg, ''), event);
     } else {
       const key = folder.replace(/Events/g, '');
       const eventFiles = fs.readdirSync(`./Files/Events/${folder}`);
+
       eventFiles.forEach((file) => {
         if (file.endsWith('.js') && file.startsWith(key)) {
           const event = require(`../Events/${folder}/${file}`);
           event.path = `../Events/${folder}/${file}`;
+
           events.set(file.replace(reg, ''), event);
         } else if (file.startsWith(key) && !file.endsWith('.js')) {
           fs.readdirSync(`./Files/Events/${folder}/${file}`).forEach((eventFolderFile) => {
             if (`${eventFolderFile}`.endsWith('.js') && `${eventFolderFile}`.startsWith(key)) {
               const event = require(`../Events/${folder}/${file}/${eventFolderFile}`);
               event.path = `../Events/${folder}/${file}/${eventFolderFile}`;
+
               events.set(eventFolderFile.replace(reg, ''), event);
             }
           });
@@ -124,14 +131,15 @@ const getEvents = () => {
 
 const getClient = () => {
   const client = new Discord.Client({
+    failIfNotExists: false,
     shards: 'auto',
     partials: ['MESSAGE', 'REACTION', 'CHANNEL', 'USER', 'GUILD_MEMBER'],
     intents: new Discord.Intents(14335),
+
     allowedMentions: {
       parse: ['users', 'roles'],
       repliedUser: false,
     },
-    failIfNotExists: false,
 
     commands: getCommands(),
     events: getEvents(),
