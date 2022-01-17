2f8e6d6e1258a8df937cb85750e03e230b65ab1a
Lars_und_so <larsundso3@gmail.com>
2022-01-17 06:05:58 +0100

    fixed massban

--- Files/Commands/massban.js
+++ Files/Commands/massban.js
@@ -14,23 +14,27 @@ module.exports = {
     const users = [];
     const failed = [];
     const reason = [];
-    const bans = await msg.guild.fetchBans(true).catch(() => {});
-    args.map(async (rawArg) => {
+
+    await msg.guild.members.fetch().catch(() => {});
+
+    args.forEach((rawArg) => {
       const arg = rawArg.toLowerCase();
-      if (Number.isNaN(arg)) {
+
+      if (Number.isNaN(+arg)) {
         if (arg.includes('<@') && arg.includes('>')) {
           const userID = arg.replace(/\D+/g, '');
-          const user = await msg.client.users.fetch(userID).catch(() => {
-            failed.push(arg);
-          });
+          const user = msg.client.users.cache.get(userID);
+
           if (user && user.id) {
-            user.source = 'massban';
-            user.guild = msg.guild;
-            const bannedUser = bans.find((thisBans) => thisBans.user.id === user.id);
+            const bannedUser = msg.guild.bans.cache.find(
+              (thisBans) => thisBans.user.id === user.id,
+            );
+
             if (bannedUser) {
               failed.push(`${arg} ${msg.lan.already}`);
             } else if (user.id !== msg.author.id) {
-              const member = await msg.guild.members.fetch(user.id).catch(() => {});
+              const member = msg.guild.members.cache.get(user.id);
+
               if (member) {
                 if (+msg.member.roles.highest.position > +member.roles.highest.position) {
                   if (+msg.member.roles.highest.position !== +member.roles.highest.position) {
@@ -40,18 +44,18 @@ module.exports = {
                 } else failed.push(`${arg} ${msg.lan.youCant}`);
               } else users.push(user);
             } else failed.push(`${arg} ${msg.lan.selfban}`);
-          } else failed.push(arg);
+          } else users.push(userID);
         } else reason.push(arg);
       } else {
-        const user = await msg.client.users.fetch(arg).catch(() => {
-          failed.push(arg);
-        });
+        const user = msg.client.users.cache.get(arg);
         if (user && user.id) {
-          const bannedUser = bans.find((thisBans) => thisBans.user.id === arg);
+          const bannedUser = msg.guild.bans.cache.find((thisBans) => thisBans.user.id === arg);
+
           if (bannedUser) {
             failed.push(`${arg} ${msg.lan.already}`);
           } else if (user.id !== msg.author.id) {
-            const member = await msg.guild.members.fetch(user.id).catch(() => {});
+            const member = msg.guild.members.cache.get(user.id);
+
             if (member) {
               if (+msg.member.roles.highest.position > +member.roles.highest.position) {
                 if (+msg.member.roles.highest.position !== +member.roles.highest.position) {
@@ -61,9 +65,10 @@ module.exports = {
               } else failed.push(`${arg} ${msg.lan.youCant}`);
             } else users.push(user);
           } else failed.push(`${arg} ${msg.lan.selfban}`);
-        } else failed.push(arg);
+        } else users.push(arg);
       }
     });
+
     let banreason = '';
     if (reason.length === 0) banreason = msg.lan.reason;
     else {
@@ -71,40 +76,45 @@ module.exports = {
         banreason += ` ${word}`;
       });
     }
+
     const con = msg.client.constants.commands.massban;
     const uniqueUsers = users.filter((item, pos, self) => self.indexOf(item) === pos);
     const uniqueFails = failed.filter((item, pos, self) => self.indexOf(item) === pos);
     const descS = [];
     const descF = [];
+
     if (uniqueUsers.length !== 0) {
       const replyEmbed = new Discord.MessageEmbed()
         .setColor(con.success)
         .setDescription(msg.lan.descS)
-        .setAuthor(
-          msg.lan.descSLoading,
-          msg.client.constants.loadingLink,
-          msg.client.constants.standard.invite,
-        )
+        .setAuthor({
+          name: msg.lan.descSLoading,
+          iconURL: msg.client.constants.emotes.loadingLink,
+          url: msg.client.constants.standard.invite,
+        })
         .setTimestamp();
       const m = await msg.client.ch.reply(msg, replyEmbed);
-      uniqueUsers.forEach(async (user) => {
+      uniqueUsers.forEach(async (user, i) => {
         const ban = await msg.guild.bans
           .create(user, {
             days: 1,
             reason: msg.client.ch.stp(msg.lan.banReason, { user: msg.author, reason: banreason }),
           })
-          .catch(() => failed.push(`${user.id} ${msg.lan.ohno}`));
+          .catch(() => {
+            uniqueUsers.splice(i, 1);
+            uniqueFails.push(`${user.id ? user.id : user} ${msg.lan.ohno}`);
+          });
         if (ban) {
-          descS.push(`<@${user.id}>`);
+          descS.push(`<@${user.id ? user.id : user}>`);
           if (`${descS}`.length > 2048)
             replyEmbed.setDescription(
               msg.client.ch.stp(msg.lan.sBannedUsers, { amount: descS.length }),
             );
-          else replyEmbed.setDescription(`\u200b${descS}`);
+          else replyEmbed.setDescription(`\u200b${descS.join(' ')}`);
         }
       });
       const editIntervalS = setInterval(async () => {
-        if (descS.length !== uniqueUsers.length) await m.edit(replyEmbed);
+        if (descS.length !== uniqueUsers.length) await m.edit({ embeds: [replyEmbed] });
       }, 5000);
       const intervalS = setInterval(async () => {
         if (descS.length === uniqueUsers.length) {
@@ -112,79 +122,93 @@ module.exports = {
             replyEmbed.setDescription(
               msg.client.ch.stp(msg.lan.sBannedUsers, { amount: descS.length }),
             );
-          else replyEmbed.setDescription(`\u200b${descS}`);
-          replyEmbed.setAuthor(
-            msg.lan.finish,
-            msg.client.constants.tickLink,
-            msg.client.constants.standard.invite,
-          );
+          else replyEmbed.setDescription(`\u200b${descS.join(' ')}`);
+          replyEmbed.setAuthor({
+            name: msg.lan.finish,
+            iconURL: msg.client.constants.emotes.tickLink,
+            url: msg.client.constants.standard.invite,
+          });
           clearInterval(intervalS);
           clearInterval(editIntervalS);
           setTimeout(() => {
             clearInterval(interval);
           }, 3600000);
           setTimeout(() => {
-            m.edit(replyEmbed);
+            if (!descS.length) m.delete();
+            else m.edit({ embeds: [replyEmbed] });
           }, 2000);
         }
       }, 1000);
     }
+
     if (uniqueFails.length !== 0) {
       const replyEmbed = new Discord.MessageEmbed()
         .setColor(con.fail)
         .setDescription(msg.lan.descF)
-        .setAuthor(
-          msg.lan.descFLoading,
-          msg.client.constants.crossLink,
-          msg.client.constants.standard.invite,
-        )
+        .setAuthor({
+          name: msg.lan.descFLoading,
+          iconURL: msg.client.constants.emotes.loadingLink,
+          url: msg.client.constants.standard.invite,
+        })
         .setTimestamp();
       const m = await msg.client.ch.reply(msg, replyEmbed);
+
       uniqueFails.forEach(async (fail) => {
         descF.push(`${fail}\n`);
         if (`${descF}`.length > 2048)
           replyEmbed.setDescription(
             msg.client.ch.stp(msg.lan.fBannedUsers, { amount: descF.length }),
           );
-        else replyEmbed.setDescription(`\u200b${descF}`);
+        else replyEmbed.setDescription(`\u200b${descF.join('')}`);
       });
+
       const editintervalF = setInterval(async () => {
-        if (descF.length !== uniqueFails.length) await m.edit(replyEmbed);
+        if (descF.length !== uniqueFails.length) await m.edit({ embeds: [replyEmbed] });
       }, 5000);
+
       const intervalF = setInterval(async () => {
         if (descF.length === uniqueFails.length) {
           if (`${descF}`.length > 2048)
             replyEmbed.setDescription(
               msg.client.ch.stp(msg.lan.fBannedUsers, { amount: descF.length }),
             );
-          else replyEmbed.setDescription(`\u200b${descF}`);
-          replyEmbed.setAuthor(
-            msg.lan.failed,
-            msg.client.constants.crossLink,
-            msg.client.constants.standard.invite,
-          );
+          else replyEmbed.setDescription(`\u200b${descF.join('')}`);
+          replyEmbed.setAuthor({
+            name: msg.lan.failed,
+            iconURL: msg.client.constants.emotes.crossLink,
+            url: msg.client.constants.standard.invite,
+          });
+
           clearInterval(intervalF);
           setTimeout(() => {
             clearInterval(interval);
           }, 3600000);
+
           clearInterval(editintervalF);
           setTimeout(() => {
-            m.edit(replyEmbed);
+            if (!descF.length) m.delete();
+            else m.edit({ embeds: [replyEmbed] });
           }, 2000);
         }
       }, 1000);
     }
+
     const interval = setInterval(async () => {
       if (uniqueUsers.length === descS.length && uniqueFails.length === descF.length) {
         const logembed = new Discord.MessageEmbed()
-          .setAuthor(
-            msg.client.ch.stp(msg.lan.log.author, { user: msg.author, amount: uniqueUsers.length }),
-          )
+          .setAuthor({
+            name: msg.client.ch.stp(msg.lan.log.author, {
+              user: msg.author,
+              amount: uniqueUsers.length,
+            }),
+          })
           .setDescription(msg.client.ch.stp(msg.lan.log.desc, { amount: uniqueUsers.length }))
-          .setField(msg.language.reason, banreason)
+          .addField(msg.language.reason, banreason)
           .setColor(con.log.color)
           .setTimestamp();
-        const path = await msg.client.ch.txtFileWriter(uniqueUsers);
+
+        const arr = uniqueUsers.map((user) => `ID: ${user.id} | Tag: ${user.tag}`);
+        const path = await msg.client.ch.txtFileWriter(msg, arr, this.name);
         if (msg.logchannels && msg.logchannels.length !== 0 && uniqueUsers.length !== 0) {
           if (path) {
             msg.logchannels.forEach((c) => {
--- Files/Events/readyEvents/ready.js
+++ Files/Events/readyEvents/ready.js
@@ -1,4 +1,3 @@
-
 // const { statcord } = require('../../BaseClient/Statcord');
 
 module.exports = {
@@ -8,17 +7,13 @@ module.exports = {
     const client = require('../../BaseClient/DiscordClient');
     console.log(`|Logged in as Ayako\n|-> Bot: ${client.user.tag}`);
     console.log(`Login at ${new Date(Date.now()).toLocaleString()}`);
-    client.guilds.cache.forEach(async (guild) =>
-      client.invites.set(guild.id, await guild.invites.fetch().catch(() => {})),
-    );
-    // statcord.autopost();
 
-    require('./slashcommands').execute();
-    require('./reminder').execute();
-    require('./webhooks').execute();
-    require('./disboard').execute();
-    require('./giveaway').execute();
-    require('./separators').execute();
+    client.guilds.cache.forEach(async (guild) => {
+      client.invites.set(guild.id, await guild.invites.fetch().catch(() => {}));
+      guild.bans.fetch().catch(() => {});
+    });
+
+    // statcord.autopost();
 
     setInterval(() => {
       // require('./websiteFetcher').execute();
@@ -30,7 +25,15 @@ module.exports = {
         client.ch.query('DELETE FROM toxicitycheck;');
       }
     }, 3600000);
+
+    require('./slashcommands').execute();
+    require('./reminder').execute();
+    require('./webhooks').execute();
+    require('./disboard').execute();
+    require('./giveaway').execute();
+    require('./separators').execute();
     require('./antivirusBlocklistCacher').execute();
+
     setInterval(() => require('./TimedManagers/TimedManagerSplitter').execute(), 2000);
     setInterval(() => require('./prunelog').execute(), 120000);
     setInterval(() => require('./presence').execute(), 60000);
