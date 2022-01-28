1ba719270a08d50c588e1ad6ca85e2d786dd28d9
Lars_und_so <Larsundso3@gmail.com>
2022-01-28 03:08:20 +0100

    invite event handling

---
+++ Files/Events/inviteEvents/inviteCreate/collectionHandler.js
@@ -0,0 +1,8 @@
+module.exports = {
+  async execute(invite) {
+    const { client } = invite;
+    const { guild } = invite;
+
+    client.invites.set(guild.id, await client.ch.getErisInvites(guild));
+  },
+};
