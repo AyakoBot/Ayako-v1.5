252d95ffc47a99bfed81c10b7c2aca448fcdefb3
Lars_und_so <Larsundso3@gmail.com>
2022-01-28 01:39:40 +0100

    fix user

--- Files/Events/guildEvents/guildMemberAdd/guildMemberAdd.js
+++ Files/Events/guildEvents/guildMemberAdd/guildMemberAdd.js
@@ -1,7 +1,6 @@
-
 module.exports = {
   async execute(member) {
-    const user = await member.guild.members.fetch(member.id);
+    const { user } = await member.guild.members.fetch(member.id);
     require('./giveaway').execute(member, user);
     require('./log').execute(member, user);
     require('./welcome').execute(member, user);
