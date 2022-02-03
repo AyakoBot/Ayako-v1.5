7f6366eab5feb408f8647e4104f470cdeb5653f1
Lars_und_so <Larsundso3@gmail.com>
2022-02-03 05:13:36 +0100

    made stp a utility command

--- Files/Commands/stp.js
+++ Files/Commands/stp.js
@@ -8,8 +8,37 @@ module.exports = {
   aliases: [],
   type: 'debugging',
   async execute(msg) {
-    const returned = msg.client.ch.stp(msg.args.join(' '), { msg });
+    let returned;
 
-    msg.channel.send({ embeds: [new Discord.MessageEmbed().setDescription(returned)] });
+    try {
+      returned = msg.client.ch.stp(msg.args.join(' '), { msg });
+    } catch (e) {
+      returned = e.message;
+    }
+
+    const embed = new Discord.MessageEmbed()
+      .setDescription(returned)
+      .addField(
+        '\u200b',
+        `${msg.language.Examples}: ${msg.client.ch.makeCodeBlock(
+          `t!stp {{msg.guild.name}}\nt!stp {{msg.author.username}}\nt!stp {{msg.channel.name}}`,
+        )}`,
+      );
+
+    const warn = new Discord.MessageButton()
+      .setCustomId('uselessbutton')
+      .setLabel(msg.lan.warn)
+      .setStyle('DANGER')
+      .setDisabled(true);
+
+    const link = new Discord.MessageButton()
+      .setURL('https://discord.js.org/#/docs/discord.js/stable/class/Message')
+      .setLabel(msg.lan.button)
+      .setStyle('LINK');
+
+    msg.client.ch.reply(msg, {
+      embeds: [embed],
+      components: msg.client.ch.buttonRower([warn, link]),
+    });
   },
 };
