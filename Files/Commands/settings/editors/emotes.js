18dc6c8d5905c21cda9cfd6cfff610cc2ce91888
Lars_und_so <Larsundso3@gmail.com>
2022-02-04 08:11:59 +0100

    added emotes editor

---
+++ Files/Commands/settings/editors/emotes.js
@@ -0,0 +1,126 @@
+const Discord = require('discord.js');
+
+module.exports = {
+  key: ['emote', 'emotes'],
+  requiresInteraction: true,
+  requiresMenu: false,
+  interactionType: 'message',
+  buttons(msg, preparedData, insertedValues, required, row) {
+    let doneDisabled = true;
+
+    if (Array.isArray(insertedValues[required.assinger])) {
+      doneDisabled =
+        msg.client.ch.arrayEquals(insertedValues[required.assinger], row[required.assinger]) ||
+        (!insertedValues[required.assinger].length && required.required);
+    } else {
+      doneDisabled = !insertedValues[required.assinger];
+    }
+
+    const done = new Discord.MessageButton()
+      .setCustomId('done')
+      .setLabel(msg.language.done)
+      .setDisabled(doneDisabled)
+      .setStyle('PRIMARY');
+
+    return [[done]];
+  },
+  messageHandler(msgData, insertedValues, required) {
+    const { msg, message } = msgData;
+
+    const args = message.content
+      .split(/:+/)
+      .map((arg) => arg.replace(/\D+/g, ''))
+      .filter((arg) => !!arg.length);
+
+    switch (required.key) {
+      default: {
+        [insertedValues[required.assinger]] = args;
+        break;
+      }
+      case 'emotes': {
+        insertedValues[required.assinger] = Array.isArray(insertedValues[required.assinger])
+          ? insertedValues[required.assinger]
+          : [];
+
+        args.forEach((arg) => {
+          if (
+            insertedValues[required.assinger] &&
+            insertedValues[required.assinger].includes(arg)
+          ) {
+            const index = insertedValues[required.assinger].indexOf(arg);
+            insertedValues[required.assinger].splice(index, 1);
+          } else if (insertedValues[required.assinger] && insertedValues[required.assinger].length)
+            insertedValues[required.assinger].push(arg);
+          else insertedValues[required.assinger] = [arg];
+        });
+      }
+    }
+
+    const selected = this.getSelected(msg, insertedValues, required, required.key);
+
+    const returnEmbed = new Discord.MessageEmbed().setDescription(
+      `**${msg.language.selected}:**\n${selected?.length ? selected : msg.language.none}`,
+    );
+
+    return { returnEmbed };
+  },
+  getSelected(msg, insertedValues, required) {
+    if (insertedValues[required.assinger]) {
+      switch (required.key) {
+        default: {
+          if (insertedValues[required.assinger]) {
+            const emote = msg.client.emojis.cache.get(insertedValues[required.assinger]);
+
+            if (emote) {
+              return `${emote}`;
+            }
+            return msg.language.noAccess;
+          }
+          return msg.language.none;
+        }
+        case 'emotes': {
+          return insertedValues[required.assinger] &&
+            insertedValues[required.assinger].length &&
+            Array.isArray(insertedValues[required.assinger])
+            ? insertedValues[required.assinger]
+                .map((value) => {
+                  const emote = msg.client.emojis.cache.get(value);
+
+                  if (emote) return `${emote}`;
+                  return msg.language.noAccess;
+                })
+                .join(', ')
+            : msg.language.none;
+        }
+      }
+    }
+    return null;
+  },
+  validator: (msg, message) => {
+    const args = message.content
+      .split(/:+/)
+      .map((arg) => arg.replace(/\D+/g, ''))
+      .filter((arg) => !!arg.length);
+
+    const noAccessEmotes = args
+      .map((arg) => {
+        const emote = msg.client.emojis.cache.get(arg);
+        if (!emote) {
+          const nameArray = message.content.split(arg)[0].split(/:+/);
+          return `\`${nameArray[nameArray.length - 2]}\``;
+        }
+        return null;
+      })
+      .filter((arg) => !!arg);
+
+    if (noAccessEmotes.length) {
+      msg.client.ch
+        .reply(message, {
+          content: msg.client.ch.stp(msg.lan.lackingAccess, { emotes: noAccessEmotes.join(', ') }),
+        })
+        .then((m) => setTimeout(() => m.delete(), 10000));
+      return false;
+    }
+    return true;
+  },
+};
