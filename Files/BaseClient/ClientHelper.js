b55d541aa21e46ad36136718eea03c9929f221f5
Lars_und_so <Larsundso3@gmail.com>
2022-02-08 04:37:45 +0100

    new regex

--- Files/BaseClient/ClientHelper.js
+++ Files/BaseClient/ClientHelper.js
@@ -20,8 +20,8 @@ const regexes = {
   strReplacer1: /_/g,
   strReplacer2: /\w\S*/g,
   auditLogTransform: /~/g,
-  // eslint-disable-next-line no-control-regex
-  tester: /[^\u0000-\u00ff]/,
+  tester:
+    /([\u2700-\u27BF]|[\uE000-\uF8FF]|\uD83C[\uDC00-\uDFFF]|\uD83D[\uDC00-\uDFFF]|[\u2011-\u26FF]|\uD83E[\uDD10-\uDDFF])/g,
 };
 
 module.exports = {
