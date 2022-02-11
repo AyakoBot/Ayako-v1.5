af36dda4824d4dd12570b04fa7e15a672ff1323e
Lars_und_so <Larsundso3@gmail.com>
2022-02-11 08:40:31 +0100

    working on leveling backend

--- Files/Events/messageEvents/messageCreate/leveling.js
+++ Files/Events/messageEvents/messageCreate/leveling.js
@@ -1,8 +1,25 @@
 const Discord = require('discord.js');
-// TODO
+const StringSimilarity = require('string-similarity');
+
+const guildCooldown = new Set();
+const lastMessageGuild = new Map();
 
 const globalCooldown = new Set();
-const serverCooldown = new Set();
+const lastMessageGlobal = new Map();
+
+/*
+TODO: 
+xpmultiplier
+blchannels
+blroles
+blusers
+wlchannels
+wlroles
+xppermsg
+finish rolemode
+lvlupdeltimeout
+lvlupchannels
+*/
 
 module.exports = {
   async execute(msg) {
@@ -11,243 +28,188 @@ module.exports = {
     const language = await msg.client.ch.languageSelector(msg.guild);
 
     globalLeveling(msg);
-    serverLeveling();
+    guildLeveling(msg, language);
   },
 };
 
 const globalLeveling = async (msg) => {
-  const res = await msg.client.ch.query('SELECT * FROM levelglobal WHERE userid = $1;', [
+  if (globalCooldown.has(msg.author.id)) return;
+
+  const lastMessage = lastMessageGlobal.get(msg.author.id);
+  if (StringSimilarity.compareTwoStrings(msg.content, lastMessage) > 0.9) return;
+  lastMessageGlobal.set(msg.author.id, msg.content);
+
+  globalCooldown.add(msg.author.id);
+  setTimeout(() => {
+    globalCooldown.delete(msg.author.id);
+  }, 10000);
+
+  const newXp = Math.floor(Math.random() * 10 + 10);
+
+  const res = await msg.client.ch.query(`SELECT * FROM level WHERE type = $1 AND userid = $2;`, [
+    'global',
     msg.author.id,
   ]);
+  if (res && res.rowCount) {
+    const oldLevel = Number(res.rows[0].level);
+    const oldXp = Number(res.rows[0].xp);
+    const xp = oldXp + newXp;
+    let newLevel = oldLevel;
+    const neededXP =
+      (5 / 6) * (newLevel + 1) * (2 * (newLevel + 1) * (newLevel + 1) + 27 * (newLevel + 1) + 91);
+
+    if (xp >= neededXP) newLevel += 1;
+
+    await msg.client.ch.query(
+      `UPDATE levels SET level = $1, xp = $2 WHERE type = $3 AND userid = $4;`,
+      [newLevel, xp, 'global', msg.author.id],
+    );
+  } else {
+    await msg.client.ch.query(
+      `INSERT INTO levels (type, userid, xp, level) VALUES ($1, $2, $3, $4);`,
+      ['global', msg.author.id, newXp, 0],
+    );
+  }
 };
 
-/* 
+const guildLeveling = async (msg, language) => {
+  const isEnabled = await checkEnabled(msg);
+  if (isEnabled === false) return;
 
-    if (resG && resG.rowCount > 0) {
-      const resSpam = await msg.client.ch.query('SELECT * FROM antilevelspam WHERE userid = $1;', [
-        msg.author.id,
-      ]);
-      if (resSpam && resSpam.rowCount > 0) {
-        if (msg.content.replace(/'/g, '').replace('`', '') === resSpam.rows[0].message) return;
-      }
-      const user = msg.client.users.cache.get(resG.rows[0].userid);
-      const curXP = resG.rows[0].xp;
-      const curLvL = resG.rows[0].level;
-      let votegain;
-      if (resG.rows[0].votegain == null) votegain = 1;
-      else votegain = resG.rows[0].votegain;
-      if (user && user.id) {
-        if (!cooldown.has(msg.author.id)) {
-          cooldown.add(msg.author.id);
-          if (resSpam) {
-            if (resSpam.rows[0])
-              msg.client.ch.query('UPDATE antilevelspam SET message = $1 WHERE userid = $2;', [
-                msg.content,
-                msg.author.id,
-              ]);
-            else
-              msg.client.ch.query('INSERT INTO antilevelspam (userid, message) VALUES ($2, $1);', [
-                msg.content,
-                msg.author.id,
-              ]);
-          } else
-            msg.client.ch.query('INSERT INTO antilevelspam (userid, message) VALUES ($2, $1);', [
-              msg.content,
-              msg.author.id,
-            ]);
-          setTimeout(() => {
-            cooldown.delete(msg.author.id);
-          }, 60000);
-          const newXP = Math.floor(Math.random() * 10 + 10) * +votegain;
-          const XP = +curXP + +newXP;
-          await msg.client.ch.query('UPDATE levelglobal SET xp = $1 WHERE userid = $2;', [
-            XP,
-            user.id,
-          ]);
-          const newLevel = +curLvL + 1;
-          const neededXP = (5 / 6) * +newLevel * (2 * +newLevel * +newLevel + 27 * +newLevel + 91);
-          if (+XP > +neededXP)
-            await msg.client.ch.query('UPDATE levelglobal SET level = $1 WHERE userid = $2;', [
-              newLevel,
-              user.id,
-            ]);
-        }
-      }
-    } else
-      msg.client.ch.query('INSERT INTO levelglobal(userid, xp, level) VALUES ($1, $2, $3);', [
-        msg.author.id,
-        1,
-        0,
-      ]);
-    const resS = await msg.client.ch.query('SELECT * FROM levelsettings WHERE guildid = $1;', [
-      msg.guild.id,
-    ]);
-    let settings;
-    if (resS && resS.rowCount > 0) [settings] = resS.rows;
-    else {
-      settings = [];
-      settings.disabled = false;
-      settings.xpgain = 1;
-      settings.blchannelid = [];
-      settings.lvlupmode = 'silent';
+  if (guildCooldown.has(msg.author.id)) return;
+
+  const lastMessage = lastMessageGuild.get(msg.author.id);
+  if (StringSimilarity.compareTwoStrings(msg.content, lastMessage) > 0.9) return;
+  lastMessageGuild.set(msg.author.id, msg.content);
+
+  guildCooldown.add(msg.author.id);
+  setTimeout(() => {
+    guildCooldown.delete(msg.author.id);
+  }, 10000);
+
+  const newXp = Math.floor(Math.random() * 10 + 15);
+
+  const res = await msg.client.ch.query(`SELECT * FROM level WHERE type = $1 AND userid = $2;`, [
+    'guild',
+    msg.author.id,
+  ]);
+  if (res && res.rowCount) {
+    const oldLevel = Number(res.rows[0].level);
+    const oldXp = Number(res.rows[0].xp);
+    const xp = oldXp + newXp;
+    let newLevel = oldLevel;
+    const neededXP =
+      (5 / 6) * (newLevel + 1) * (2 * (newLevel + 1) * (newLevel + 1) + 27 * (newLevel + 1) + 91);
+
+    if (xp >= neededXP) {
+      newLevel += 1;
+      levelUp(msg, language, { oldXp, newXp: xp, newLevel, oldLevel }, isEnabled);
     }
-    if (settings.disabled === true) return;
-    if (settings.disabled === false) {
-      if (settings.blchannelid) {
-        if (settings.blchannelid.includes(msg.channel.id)) return;
-      }
-      if (!cooldownServer.has(msg.author.id)) {
-        cooldownServer.add(msg.author.id);
-        setTimeout(() => {
-          cooldownServer.delete(msg.author.id);
-        }, 60000);
-        const result = await msg.client.ch.query(
-          'SELECT * FROM levelserver WHERE userid = $1 AND guildid = $2;',
-          [msg.author.id, msg.guild.id],
-        );
-        if (result && result.rowCount > 0) {
-          if (result.rows[0].blroleid) {
-            [...result.rows[0].blroleid.entries()].forEach((id) => {
-              const role = msg.guild.roles.cache.get(id);
-              if (!role) {
-                result.rows[0].blroleid.splice(result.rows[0].blroleid.indexOf(id), 1);
-                msg.client.ch.query('UPDATE levelserver SET blroleid = $1 WHERE guildid = $2;', [
-                  result.rows[0].blroleid,
-                  msg.guild.id,
-                ]);
-              }
-            });
-          }
-          const curXP = result.rows[0].xp;
-          const curLvL = result.rows[0].level;
-          let multiplier;
-          const mRes = await msg.client.ch.query(
-            'SELECT * FROM multiroles WHERE guildid = $1;',
-            [msg.guild.id],
-          );
-          if (mRes && mRes.rowCount > 0) {
-            mRes.rows.forEach(async (row) => {
-              const role = msg.guild.roles.cache.get(row.role);
-              if (!role)
-                return msg.client.ch.query(
-                  'DELETE FROM multiroles WHERE guildid = $1 AND roleid = $2;',
-                  [msg.guild.id, row.roleid],
-                );
-              if (msg.member && msg.member.roles.cache.has(role.id)) multiplier += row.multiplier;
-              return null;
-            });
-          }
-          const newXP =
-            Math.floor(Math.random() * 10 + 15) * +settings.xpgain * multiplier ? multiplier : 1;
-          const XP = +curXP + +newXP;
-          await msg.client.ch.query(
-            'UPDATE levelserver SET xp = $1 WHERE userid = $2 AND guildid = $3;',
-            [XP, msg.author.id, msg.guild.id],
-          );
-          const newLevel = +curLvL + 1;
-          const neededXP = (5 / 6) * +newLevel * (2 * +newLevel * +newLevel + 27 * +newLevel + 91);
-          if (+XP > +neededXP) {
-            await msg.client.ch.query(
-              'UPDATE levelserver SET level = $1 WHERE userid = $2 AND guildid = $3;',
-              [newLevel, msg.author.id, msg.guild.id],
-            );
-            if (newLevel === 1) {
-              if (settings.lvlupmode === 'reactions') {
-                const LevelUpEmbed = new Discord.MessageEmbed()
-                  .setAuthor(
-                    msg.client.ch.stp(language.commands.levelsettings.levelUp.author, {
-                      user: msg.author,
-                      level: newLevel,
-                    }),
-                    msg.client.ch.displayAvatarURL(msg.author),
-                  )
-                  .setTimestamp()
-                  .setDescription(
-                    msg.client.ch.stp(language.commands.levelsettings.levelUp.description, {
-                      emote1: msg.client.constants.emotes.ayakoPeek,
-                      emote2: msg.client.constants.emotes.up,
-                    }),
-                  )
-                  .setColor(msg.client.ch.colorSelector(msg.guild ? msg.guild.me : null))
-                  .setFooter(language.commands.levelsettings.levelUp.footer);
-                const m = await msg.client.ch.send(msg.channel, LevelUpEmbed);
-                setTimeout(() => {
-                  m.delete().catch(() => {});
-                }, 10000);
-                msg.react(msg.client.emotes.ayakoPeekID).catch(() => {});
-                msg.react(msg.client.emotes.up).catch(() => {});
-                setTimeout(() => {
-                  msg.reactions.removeAll().catch(() => {});
-                }, 10000);
-              }
-            }
-            if (newLevel % 10 === 0) {
-              if (settings.lvlupmode === 'reactions') {
-                const LevelUpEmbed = new Discord.MessageEmbed()
-                  .setAuthor(
-                    msg.client.ch.stp(language.commands.levelsettings.levelUp.author, {
-                      user: msg.author,
-                      level: newLevel,
-                    }),
-                    msg.client.ch.displayAvatarURL(msg.author),
-                  )
-                  .setTimestamp()
-                  .setColor(msg.client.ch.colorSelector(msg.guild ? msg.guild.me : null))
-                  .setFooter(language.commands.levelsettings.levelUp.footer);
-                const m = await msg.client.ch.send(msg.channel, LevelUpEmbed);
-                setTimeout(() => {
-                  m.delete().catch(() => {});
-                }, 10000);
-              }
-            } else {
-              let leveluptext = msg.client.ch.stp(language.commands.levelsettings.levelUp.author, {
-                user: msg.author,
-                level: newLevel,
-              });
-              if (result.rows[0].text)
-                leveluptext = result.rows[0].text
-                  .replace('{user}', `${msg.author}`)
-                  .replace('{level}', `${newLevel}`);
-              if (settings.lvlupmode === 'reactions') {
-                msg.react(msg.client.emotes.ayakoPeekID).catch(() => {});
-                msg.react(msg.client.emotes.up).catch(() => {});
-                setTimeout(() => {
-                  msg.reactions.removeAll().catch(() => {});
-                }, 10000);
-              } else if (settings.lvlupmode === 'messages') {
-                const embed = new Discord.MessageEmbed()
-                  .setAuthor({
-                    name: `${leveluptext}`,
-                    iconURL: msg.client.ch.displayAvatarURL(msg.author),
-                  })
-                  .setColor(msg.client.ch.colorSelector(msg.guild ? msg.guild.me : null));
-                let channel;
-                if (settings.lvlupchannel) {
-                  channel = msg.client.channels.cache.get(settings.lvlupchannel);
-                  if (!channel) channel = msg.channel;
-                } else channel = msg.channel;
-                msg.client.ch.send(msg.channel, `${msg.author}`, embed).catch(() => {});
-              }
-            }
-            const resR = await msg.client.ch.query(
-              'SELECT * FROM levelroles WHERE guildid = $1 AND level < $2;',
-              [msg.guild.id, +newLevel + 1],
-            );
-            if (resR && resR.rowCount > 0) {
-              for (let i = 0; i < resR.rowCount; i += 1) {
-                const role = msg.guild.roles.cache.find((r) => r.id === resR.rows[i].roleid);
-                if (!msg.member.roles.cache.has(role.id))
-                  msg.member.roles.add(role).catch(() => {});
-              }
-            }
-          }
-        } else {
-          const newXP = Math.floor(Math.random() * 10 + 15);
-          await msg.client.ch.query(
-            'INSERT INTO levelserver(guildid, userid, xp, level) VALUES ($1, $2, $3, $4);',
-            [msg.guild.id, msg.author.id, newXP, 0],
-          );
-        }
-      }
+
+    await msg.client.ch.query(
+      `UPDATE levels SET level = $1, xp = $2 WHERE type = $3 AND userid = $4 AND guildid = $5;`,
+      [newLevel, xp, 'guild', msg.author.id, msg.guild.id],
+    );
+  } else {
+    await msg.client.ch.query(
+      `INSERT INTO levels (type, userid, xp, level, guildid) VALUES ($1, $2, $3, $4, $5);`,
+      ['guild', msg.author.id, newXp, 0, msg.guild.id],
+    );
+  }
+};
+
+const checkEnabled = async (msg) => {
+  const res = await msg.client.ch.query(`SELECT * FROM leveling WHERE guildid = $1;`, [
+    msg.guild.id,
+  ]);
+
+  if (res && res.rowCount) {
+    if (res.rows[0].active === false) return false;
+  }
+  return res;
+};
+
+const levelUp = async (msg, language, levelData, res) => {
+  switch (Number(res.rows[0].lvlupmode)) {
+    default: {
+      break;
     }
-    */
+    case 1: {
+      await doEmbed(msg, res, language, levelData);
+      break;
+    }
+    case 2: {
+      doReact(msg, res);
+      break;
+    }
+  }
+
+  roleAssign(msg, res, levelData.newLevel);
+};
+
+const roleAssign = async (msg, res, newLevel) => {
+  switch (Number(res.rows[0].rolemode)) {
+    default: {
+      break;
+    }
+    case 0: {
+      // stack
+      break;
+    }
+    case 1: {
+      // replace
+      break;
+    }
+  }
+};
+
+const doReact = async (msg, res) => {
+  const reactions = [];
+
+  if (res.lvlupemotes?.length) {
+    res.lvlupemotes.forEach((emoteID) => {
+      const emote = msg.client.emojis.cache.get(emoteID);
+      if (emote) reactions.push(emote);
+    });
+  } else {
+    msg.client.constants.standard.levelupemotes.forEach((emote) => reactions.push(emote));
+  }
+
+  const promises = reactions.map((emote) => msg.react(emote).catch(() => {}));
+  await Promise.all(promises);
+};
+
+const doEmbed = async (msg, levelRes, language, levelData) => {
+  const getDefaultEmbed = () => {
+    return new Discord.MessageEmbed()
+      .setAuthor({ name: language.leveling.author })
+      .setColor(msg.client.ch.colorSelector(msg.member));
+  };
+
+  let embed;
+
+  if (!levelRes.rows[0].embed) embed = getDefaultEmbed();
+  else {
+    const res = await msg.client.ch.query(
+      `SELECT * FROM customembeds WHERE uniquetimestamp = $1 AND guildid = $2;`,
+      [levelRes.rows[0].embed, msg.guild.id],
+    );
+    if (res && res.rowCount) {
+      const options = [
+        ['msg', msg],
+        ['user', msg.author],
+        ['newLevel', levelData.newLevel],
+        ['oldLevel', levelData.oldLevel],
+        ['newXP', levelData.newXp],
+        ['oldXP', levelData.oldXp],
+      ];
+
+      const partialEmbed = msg.client.ch.getDiscordEmbed(res.rows[0]);
+      embed = msg.client.ch.dynamicToEmbed(partialEmbed, options);
+    } else {
+      embed = getDefaultEmbed();
+    }
+  }
+
+  msg.client.ch.send({ embeds: [embed] });
+};
