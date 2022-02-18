36fb5a76df88938d663a3481d5041225a8951ef4
Larsundso <larsundso3@gmail.com>
2022-02-18 11:40:45 +0100

    channel perm sticky

--- Files/Commands/settings/categories/stickyroles.js
+++ Files/Commands/settings/categories/stickyroles.js
@@ -17,10 +17,17 @@ module.exports = {
         inline: false,
       },
       {
-        name: msg.lan.stickymode,
-        value: r.stickymode
+        name: msg.lan.stickyrolesmode,
+        value: r.stickyrolesmode
           ? `${msg.client.constants.emotes.enabled} ${msg.lan.unsticky}`
           : `${msg.client.constants.emotes.disabled} ${msg.lan.sticky}`,
+        inline: false,
+      },
+      {
+        name: r.stickyrolesmode ? msg.lan.stickyRoles : msg.lan.notStickyRoles,
+        value: `${
+          r.roles && r.roles.length ? r.roles.map((id) => ` <@&${id}>`) : msg.language.none
+        }`,
         inline: false,
       },
       {
@@ -29,10 +36,10 @@ module.exports = {
         inline: false,
       },
       {
-        name: r.stickymode ? msg.lan.stickyRoles : msg.lan.notStickyRoles,
-        value: `${
-          r.roles && r.roles.length ? r.roles.map((id) => ` <@&${id}>`) : msg.language.none
-        }`,
+        name: msg.lan.stickypermsactive,
+        value: r.stickypermsactive
+          ? `${msg.client.constants.emotes.enabled} ${msg.language.enabled}`
+          : `${msg.client.constants.emotes.disabled} ${msg.language.disabled}`,
         inline: false,
       },
     );
@@ -43,14 +50,18 @@ module.exports = {
       .setCustomId(msg.lan.edit.stickyrolesactive.name)
       .setLabel(msg.lan.stickyrolesactive)
       .setStyle(r.active ? 'SUCCESS' : 'DANGER');
-    const stickymode = new Discord.MessageButton()
-      .setCustomId(msg.lan.edit.stickymode.name)
-      .setLabel(msg.lan.stickymode)
+    const stickyrolesmode = new Discord.MessageButton()
+      .setCustomId(msg.lan.edit.stickyrolesmode.name)
+      .setLabel(msg.lan.stickyrolesmode)
       .setStyle('SECONDARY');
     const roles = new Discord.MessageButton()
       .setCustomId(msg.lan.edit.roles.name)
       .setLabel(msg.lan.roles)
       .setStyle('PRIMARY');
-    return [[stickyrolesactive], [stickymode, roles]];
+    const stickypermsactive = new Discord.MessageButton()
+      .setCustomId(msg.lan.edit.stickypermsactive.name)
+      .setLabel(msg.lan.stickypermsactive)
+      .setStyle(r.active ? 'SUCCESS' : 'DANGER');
+    return [[stickyrolesactive], [stickyrolesmode, roles], [stickypermsactive]];
   },
 };
--- Files/Events/guildEvents/guildMemberAdd/sticky.js
+++ Files/Events/guildEvents/guildMemberAdd/sticky.js
@@ -1,31 +1,35 @@
 module.exports = {
   execute: async (member, user) => {
-    const memberRes = await member.client.ch.query(
-      `SELECT * FROM stickymembers WHERE userid = $1 AND guildid = $2;`,
-      [user.id, member.guild.id],
-    );
-
-    if (!memberRes || !memberRes.rowCount) return;
-
-    const settingsRes = await member.client.ch.query(
-      `SELECT * FROM sticky WHERE guildid = $1 AND active = true;`,
-      [member.guild.id],
-    );
-
-    if (!settingsRes || !settingsRes.rowCount) return;
-
-    const rolesToAdd = getRoles(
-      settingsRes.rows[0].stickymode,
-      settingsRes.rows[0].roles,
-      memberRes.rows[0].roles,
-    );
-
-    await member.client.ch.query(`DELETE FROM stickymembers WHERE userid = $1 AND guildid = $2;`, [
-      user.id,
-      member.guild.id,
-    ]);
-    if (rolesToAdd.length) await member.roles.add(rolesToAdd);
+    stickyroles(member, user);
   },
+};
+
+const stickyroles = async (member, user) => {
+  const memberRes = await member.client.ch.query(
+    `SELECT * FROM stickymembers WHERE userid = $1 AND guildid = $2;`,
+    [user.id, member.guild.id],
+  );
+
+  if (!memberRes || !memberRes.rowCount) return;
+
+  const settingsRes = await member.client.ch.query(
+    `SELECT * FROM sticky WHERE guildid = $1 AND active = true;`,
+    [member.guild.id],
+  );
+
+  if (!settingsRes || !settingsRes.rowCount) return;
+
+  const rolesToAdd = getRoles(
+    settingsRes.rows[0].stickyrolesmode,
+    settingsRes.rows[0].roles,
+    memberRes.rows[0].roles,
+  );
+
+  await member.client.ch.query(`DELETE FROM stickymembers WHERE userid = $1 AND guildid = $2;`, [
+    user.id,
+    member.guild.id,
+  ]);
+  if (rolesToAdd.length) await member.roles.add(rolesToAdd);
 };
 
 const getRoles = (mode, roles, memberRoles) => {
