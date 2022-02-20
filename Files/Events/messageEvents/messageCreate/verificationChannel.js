fea25279c57d55b497f8aeb2afd0ff44047f5005
Larsundso <larsundso3@gmail.com>
2022-02-20 08:14:38 +0100

    added start channel message

--- Files/Events/messageEvents/messageCreate/verificationChannel.js
+++ Files/Events/messageEvents/messageCreate/verificationChannel.js
@@ -3,7 +3,7 @@ module.exports = {
     if (!msg.guild || msg.author.id === msg.client.user.id) return;
 
     const res = await msg.client.ch.query(
-      `SELECT * FROM verification WHERE guildid = $1 AND startchannel = $2;`,
+      `SELECT * FROM verification WHERE guildid = $1 AND startchannel = $2 AND deletestartchmsgs = true AND active = true;`,
       [msg.guild.id, msg.channel.id],
     );
 
