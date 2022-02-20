4af321812168cea4550beadf04d3750935b4620d
Larsundso <larsundso3@gmail.com>
2022-02-20 08:44:20 +0100

    removed old stuff

--- Files/Events/readyEvents/TimedManagers/TimedManagerSplitter.js
+++ Files/Events/readyEvents/TimedManagers/TimedManagerSplitter.js
@@ -2,7 +2,7 @@ module.exports = {
   execute() {
     require('./separatorControl').execute();
     require('./antivirusClear').execute();
-    // require('./disboard').execute();
+    require('./disboard').execute();
     // require('./voteReminder').execute();
     require('./mute').execute();
     // require('./stats').execute();
