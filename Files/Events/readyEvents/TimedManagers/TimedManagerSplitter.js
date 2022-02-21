8e1b67bea72908226e1a3f5c5de3202046b4bb8d
Larsundso <larsundso3@gmail.com>
2022-02-21 17:10:46 +0100

    finished warn expiry

--- Files/Events/readyEvents/TimedManagers/TimedManagerSplitter.js
+++ Files/Events/readyEvents/TimedManagers/TimedManagerSplitter.js
@@ -2,8 +2,8 @@ module.exports = {
   execute() {
     require('./separatorControl').execute();
     require('./antivirusClear').execute();
-    // require('./voteReminder').execute();
     require('./mute').execute();
+    require('./expiry').execute();
     // require('./stats').execute();
   },
 };
