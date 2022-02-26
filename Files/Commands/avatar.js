0408c1eced103701680ee388d712ff4d6ffa974b
Larsundso <larsundso3@gmail.com>
2022-02-26 19:39:01 +0100

    fixed av

--- Files/Commands/avatar.js
+++ Files/Commands/avatar.js
@@ -11,16 +11,17 @@ module.exports = {
     const user = msg.args[0]
       ? await msg.client.users.fetch(msg.args[0].replace(/\D+/g, ''))
       : msg.author;
-    const avatarEmbed = new Discord.MessageEmbed()
-      .setAuthor(
-        msg.client.ch.stp(msg.lan.avatarOf, { user }),
-        msg.client.constants.standard.image,
-        msg.client.ch.displayAvatarURL(user),
-      )
+
+    const embed = new Discord.MessageEmbed()
+      .setAuthor({
+        name: msg.client.ch.stp(msg.lan.avatarOf, { user }),
+        iconURL: msg.client.constants.standard.image,
+        url: msg.client.ch.displayAvatarURL(user),
+      })
       .setImage(msg.client.ch.displayAvatarURL(user))
       .setTimestamp()
       .setColor(msg.client.ch.colorSelector(msg.guild ? msg.guild.me : null))
       .setFooter({ text: msg.client.ch.stp(msg.language.requestedBy, { user: msg.author }) });
-    msg.channel.send(avatarEmbed);
+    msg.client.ch.reply(msg, { embeds: [embed] });
   },
 };
