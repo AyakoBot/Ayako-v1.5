fce6f9023947ead403e889c153b06163b83ab1c5
Larsundso <larsundso3@gmail.com>
2022-03-02 15:49:37 +0100

    added server avatar commands

--- Files/Events/messageEvents/messageCreate/commandHandler.js
+++ Files/Events/messageEvents/messageCreate/commandHandler.js
@@ -21,7 +21,7 @@ module.exports = {
     else return;
     if (!prefix) return;
     msg.language = await msg.client.ch.languageSelector(msg.guild);
-    const args = msg.content.replace(new RegExp('\\n', 'g'), ' ').slice(prefix.length).split(/ +/);
+    const args = msg.content.replace(/\\n/g, ' ').slice(prefix.length).split(/ +/);
     const commandName = args.shift().toLowerCase();
     let command =
       msg.client.commands.get(commandName) ||
