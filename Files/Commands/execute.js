3b80c63049376c862637886e8dfaa75a0ab3dd8c
Larsundso <larsundso3@gmail.com>
2022-03-03 13:33:34 +0100

    added synclevel (not finished)

--- Files/Commands/execute.js
+++ Files/Commands/execute.js
@@ -12,7 +12,6 @@ const superagent = require('superagent');
 const Discord = require('discord.js');
 const jobs = require('node-schedule');
 const ms = require('ms');
-const math = require('math.js');
 
 module.exports = {
   name: 'execute',
@@ -20,58 +19,5 @@ module.exports = {
   perm: 0n,
   dm: true,
   takesFirstArg: false,
-  execute: async (msg) => {
-    let x;
-    const y = 1150;
-    {
-      (x =
-        (243000 + 8100 * y + 75 * Math.sqrt((11664 * y) ** (2 + 699840 * y - 10099275))) **
-        (1 / 3 / 30 +
-          325 /
-            (2 *
-              (243000 + 8100 * y + 75 * Math.sqrt((11664 * y) ** (2 + 699840 * y - 10099275)))) **
-              (1 / 3) -
-          9 / 2)),
-        (x =
-          (-(243000 + 8100 * y + 75 * Math.sqrt((11664 * y) ** (2 + 699840 * y - 10099275)))) **
-          (1 / 3 / 60 -
-            325 /
-              (4 *
-                (243000 + 8100 * y + 75 * Math.sqrt((11664 * y) ** (2 + 699840 * y - 10099275)))) **
-                (1 / 3) -
-            9 / 2 -
-            (Math.sqrt(3) *
-              (243000 + 8100 * y + 75 * Math.sqrt((11664 * y) ** (2 + 699840 * y - 10099275))) **
-                (1 / 3 / 30 -
-                  325 /
-                    (2 *
-                      (243000 +
-                        8100 * y +
-                        75 * Math.sqrt((11664 * y) ** (2 + 699840 * y - 10099275)))) **
-                      (1 / 3)) *
-              math.i) /
-              2)),
-        (x =
-          (-(243000 + 8100 * y + 75 * Math.sqrt((11664 * y) ** (2 + 699840 * y - 10099275)))) **
-          (1 / 3 / 60 -
-            325 /
-              (4 *
-                (243000 + 8100 * y + 75 * Math.sqrt((11664 * y) ** (2 + 699840 * y - 10099275)))) **
-                (1 / 3) -
-            9 / 2 +
-            (Math.sqrt(3) *
-              (243000 + 8100 * y + 75 * Math.sqrt((11664 * y) ** (2 + 699840 * y - 10099275))) **
-                (1 / 3 / 30 -
-                  325 /
-                    (2 *
-                      (243000 +
-                        8100 * y +
-                        75 * Math.sqrt((11664 * y) ** (2 + 699840 * y - 10099275)))) **
-                      (1 / 3)) *
-              math.i) /
-              2));
-    }
-
-    console.log(x);
-  },
+  execute: async (msg) => {},
 };
