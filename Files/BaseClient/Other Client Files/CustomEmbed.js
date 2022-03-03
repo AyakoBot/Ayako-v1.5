c1030f7f632ba284c7a1ef375340650a2e4cf220
Larsundso <larsundso3@gmail.com>
2022-03-03 17:57:53 +0100

    added custom Embed

---
+++ Files/BaseClient/Other Client Files/CustomEmbed.js
@@ -0,0 +1,34 @@
+const Discord = require('discord.js');
+
+class CustomEmbed extends Discord.UnsafeEmbed {
+  constructor(guild, oldEmbed) {
+    super();
+
+    this.data = oldEmbed.data;
+
+    if (oldEmbed) {
+      if (oldEmbed.author && !oldEmbed.author.link) {
+        this.setAuthor({
+          url: guild.client.constants.standard.invite,
+          name: oldEmbed.author.name,
+          iconURL: oldEmbed.author.iconURL,
+        });
+      }
+
+      if (!oldEmbed.color) {
+        this.setColor(
+          guild && guild.me ? guild.me.displayColor : guild.client.constants.standard.color,
+        );
+      }
+    }
+  }
+
+  /**
+   * @param {{ data: any; }} oldEmbed
+   */
+  set data(oldEmbed) {
+    this.data = oldEmbed.data;
+  }
+}
+
+module.exports = CustomEmbed;
