da39a2ccc455cad7a25c3e785297f9a8d6a8c168
Larsundso <larsundso3@gmail.com>
2022-03-10 13:05:10 +0100

    edit fix

--- Files/Commands/ping.js
+++ Files/Commands/ping.js
@@ -12,6 +12,8 @@ module.exports = {
     if (msg.editedTimestamp) ping = m.createdTimestamp - msg.editedTimestamp;
     else ping = m.createdTimestamp - msg.createdTimestamp;
     const { heartbeat } = (await msg.client.ch.query('SELECT heartbeat FROM stats;')).rows[0];
-    m.edit(`🏓 \n**Response Time:** ${ping}ms\n**Last Heartbeat:** ${heartbeat}ms`).catch(() => {});
+    m?.edit(`🏓 \n**Response Time:** ${ping}ms\n**Last Heartbeat:** ${heartbeat}ms`).catch(
+      () => {},
+    );
   },
 };
