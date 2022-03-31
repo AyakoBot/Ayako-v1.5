1fb9ef2e379548482050b9889d3f9be84c91787b
Larsundso <larsundso3@gmail.com>
2022-03-31 14:49:26 +0200

    dont notify about afk on afk remove

--- Files/Events/messageEvents/messageCreate/afk.js
+++ Files/Events/messageEvents/messageCreate/afk.js
@@ -20,6 +20,7 @@ module.exports = {
     if (res && res.rowCount > 0) {
       if (+res.rows[0].since + 60000 < Date.now()) {
         if (checkedMsg?.command.name === 'afk') return;
+
         const embed = new Builders.UnsafeEmbedBuilder()
           .setColor(msg.client.ch.colorSelector(msg.guild.me))
           .setDescription(
@@ -39,36 +40,42 @@ module.exports = {
             m.delete().catch(() => {});
           });
         }
-        msg.client.ch.query('DELETE FROM afk WHERE userid = $1;', [msg.author.id]);
+        msg.client.ch.query('DELETE FROM afk WHERE userid = $1 AND guildid = $2;', [
+          msg.author.id,
+          msg.guild.id,
+        ]);
       }
     }
-    let mentioned = msg.mentions.users;
-    mentioned = mentioned.map((o) => o);
-    mentioned.forEach(async (mention) => {
-      const afkRes = await msg.client.ch.query(
-        'SELECT * FROM afk WHERE userid = $1 AND guildid = $2;',
-        [mention.id, msg.guild.id],
-      );
-      if (afkRes && afkRes.rowCount > 0) {
-        const embed = new Builders.UnsafeEmbedBuilder()
-          .setColor(msg.client.ch.colorSelector(msg.guild.me))
-          .setDescription(
-            `${
-              afkRes.rows[0].text
-                ? msg.client.ch.stp(language.commands.afk.afkText2, {
-                    username: mention.username,
-                    slice: afkRes.rows[0].text,
-                  })
-                : msg.client.ch.stp(language.commands.afk.afkText, { username: mention.username })
-            } ${moment
-              .duration(+afkRes.rows[0].since - Date.now())
-              .format(
-                ` D [${language.time.days}], H [${language.time.hours}], m [${language.time.minutes}], s [${language.time.seconds}]`,
-              )
-              .replace(/-/g, '')}`,
-          );
-        msg.client.ch.reply(msg, { embeds: [embed] });
-      }
-    });
+    if (checkedMsg?.command.name === 'unafk') return;
+
+    msg.mentions.users
+      .map((o) => o)
+      .forEach(async (mention) => {
+        const afkRes = await msg.client.ch.query(
+          'SELECT * FROM afk WHERE userid = $1 AND guildid = $2;',
+          [mention.id, msg.guild.id],
+        );
+
+        if (afkRes && afkRes.rowCount > 0) {
+          const embed = new Builders.UnsafeEmbedBuilder()
+            .setColor(msg.client.ch.colorSelector(msg.guild.me))
+            .setDescription(
+              `${
+                afkRes.rows[0].text
+                  ? msg.client.ch.stp(language.commands.afk.afkText2, {
+                      username: mention.username,
+                      slice: afkRes.rows[0].text,
+                    })
+                  : msg.client.ch.stp(language.commands.afk.afkText, { username: mention.username })
+              } ${moment
+                .duration(+afkRes.rows[0].since - Date.now())
+                .format(
+                  ` D [${language.time.days}], H [${language.time.hours}], m [${language.time.minutes}], s [${language.time.seconds}]`,
+                )
+                .replace(/-/g, '')}`,
+            );
+          msg.client.ch.reply(msg, { embeds: [embed] });
+        }
+      });
   },
 };
