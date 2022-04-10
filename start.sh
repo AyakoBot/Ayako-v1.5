b643c7420a8f64d31efdb96fea7d3cce94c371da
Larsundso <Larsundso3@gmail.com>
2022-04-11 00:59:43 +0200

    node start

--- start.sh
+++ start.sh
@@ -1,6 +1,2 @@
-function start {
-    pm2 start --node-args="--trace-deprecation --trace-warnings --max-old-space-size=8192" ./Ayako-v1.5.js
-    sleep 0s
-}
-start
+screen ./nodestart.sh -S Ayako-v1.5
 exit
