7b4048c1e8894ed322c776075246bdb6fbdeec2a
Larsundso <Larsundso3@gmail.com>
2022-04-12 20:12:09 +0200

    finished

--- Files/Events/messageEvents/messageReactionAdd/ReactionRoles.js
+++ Files/Events/messageEvents/messageReactionAdd/ReactionRoles.js
@@ -1,25 +1,91 @@
-const client = require('../../../BaseClient/DiscordClient');
-
 module.exports = {
   async execute(reaction, user) {
-    return;
+    if (user.id === reaction.client.user.id) return;
+    if (!reaction.message.guild) return;
 
-    if (user.id === client.user.id) return;
-    const { ch } = client;
-    const { guild } = reaction.message;
-    const isUnicode = ch.containsNonLatinCodepoints(reaction.emoji.name);
-    const res = await ch.query('SELECT * FROM reactionroles WHERE msgid = $1 AND emoteid = $2;', [
-      reaction.message.id,
-      isUnicode ? reaction.emoji.name : reaction.emoji.id,
-    ]);
-    if (res && res.rowCount > 0) {
-      const member = await guild.members.fetch(user.id);
-      for (let i = 0; i < res.rowCount; i += 1) {
-        if (member) {
-          const role = guild.roles.cache.get(res.rows[i].roleid);
-          if (!member.roles.cache.has(res.rows[i].roleid)) member.roles.add(role).catch(() => {});
-        }
-      }
+    const emoteIdentifier = reaction.client.ch.containsNonLatinCodepoints(reaction.emoji.name)
+      ? reaction.emoji.name
+      : reaction.emoji.id;
+
+    const reactionRow = await getReactionRow(reaction, emoteIdentifier);
+    if (!reactionRow) return;
+
+    const baseRow = await getBaseRow(reaction, reactionRow);
+    if (!baseRow) return;
+
+    const member = await reaction.message.guild.members.fetch(user.id).catch(() => {});
+    if (!member) return;
+
+    const relatedReactions = await getRelatedReactions(reaction, baseRow, reactionRow);
+    const hasAnyOfRelated = getHasAnyOfRelated(relatedReactions, member);
+
+    const hasOne = member.roles.cache.some((r) => reactionRow.roles.includes(r.id));
+
+    if (!hasOne) {
+      giveRoles(reaction, reactionRow, baseRow, hasAnyOfRelated, member);
     }
   },
+};
+
+const getBaseRow = async (reaction, reactionRow) => {
+  const res = await reaction.client.ch.query(
+    `SELECT * FROM rrsettings WHERE messagelink = $1 AND guildid = $2;`,
+    [reactionRow.messagelink, reaction.message.guild.id],
+  );
+
+  if (!res || !res.rowCount) return null;
+  return res.rows[0];
+};
+
+const getRelatedReactions = async (reaction, baseRow, reactionRow) => {
+  const res = await reaction.client.ch.query(
+    `SELECT * FROM rrbuttons WHERE guildid = $1 AND messagelink = $2 AND uniquetimestamp != $3;`,
+    [reaction.message.guild.id, baseRow.messagelink, reactionRow.uniquetimestamp],
+  );
+
+  if (!res || !res.rowCount) return null;
+  return res.rows;
+};
+
+const getHasAnyOfRelated = (relatedReactions, member) => {
+  if (!relatedReactions || !relatedReactions.length) return false;
+
+  let hasAnyOfRelated = false;
+
+  relatedReactions.forEach((row) => {
+    row.roles.forEach((role) => {
+      if (hasAnyOfRelated) return;
+      if (member.roles.cache.has(role)) hasAnyOfRelated = true;
+    });
+  });
+
+  return hasAnyOfRelated;
+};
+
+const giveRoles = async (reaction, reactionRow, baseRow, hasAnyOfRelated, member) => {
+  const rolesToAdd = [];
+
+  reactionRow.roles.forEach((rID) => {
+    if (!member.roles.cache.has(rID)) rolesToAdd.push(rID);
+  });
+
+  if (hasAnyOfRelated && baseRow.anyroles && baseRow.anyroles.length) {
+    baseRow.anyroles.forEach((rID) => {
+      if (!member.roles.cache.has(rID)) rolesToAdd.push(rID);
+    });
+  }
+
+  if (rolesToAdd.length) {
+    await member.roles.add(rolesToAdd).catch(() => {});
+  }
+};
+
+const getReactionRow = async (reaction, emoteIdentifier) => {
+  const res = await reaction.client.ch.query(
+    'SELECT * FROM rrreactions WHERE messagelink = $1 AND emoteid = $2 AND guildid = $3;',
+    [reaction.message.url, emoteIdentifier, reaction.message.guild.id],
+  );
+
+  if (!res || !res.rowCount) return [];
+  return res.rows[0];
 };
--- Files/Events/messageEvents/messageReactionRemove.js
+++ Files/Events/messageEvents/messageReactionRemove.js
@@ -1,23 +1,91 @@
 module.exports = {
   async execute(reaction, user) {
-    return;
-    const { client } = reaction;
-    if (user.id === client.user.id) return;
-    const { ch } = client;
-    const { guild } = reaction.message;
-    const isUnicode = ch.containsNonLatinCodepoints(reaction.emoji.name);
-    const res = await ch.query('SELECT * FROM reactionroles WHERE msgid = $1 AND emoteid = $2;', [
-      reaction.message.id,
-      isUnicode ? reaction.emoji.name : reaction.emoji.id,
-    ]);
-    if (res && res.rowCount > 0) {
-      res.rows.forEach(async (row) => {
-        const member = await guild.members.fetch(user.id);
-        if (member) {
-          const role = guild.roles.cache.get(row.roleid);
-          if (member.roles.cache.has(row.roleid)) member.roles.remove(role).catch(() => {});
-        }
-      });
+    if (user.id === reaction.client.user.id) return;
+    if (!reaction.message.guild) return;
+
+    const emoteIdentifier = reaction.client.ch.containsNonLatinCodepoints(reaction.emoji.name)
+      ? reaction.emoji.name
+      : reaction.emoji.id;
+
+    const reactionRow = await getReactionRow(reaction, emoteIdentifier);
+    if (!reactionRow) return;
+
+    const baseRow = await getBaseRow(reaction, reactionRow);
+    if (!baseRow) return;
+
+    const member = await reaction.message.guild.members.fetch(user.id).catch(() => {});
+    if (!member) return;
+
+    const relatedReactions = await getRelatedReactions(reaction, baseRow, reactionRow);
+    const hasAnyOfRelated = getHasAnyOfRelated(relatedReactions, member);
+
+    const hasOne = member.roles.cache.some((r) => reactionRow.roles.includes(r.id));
+
+    if (hasOne) {
+      takeRoles(reaction, reactionRow, baseRow, hasAnyOfRelated, member);
     }
   },
+};
+
+const getBaseRow = async (reaction, reactionRow) => {
+  const res = await reaction.client.ch.query(
+    `SELECT * FROM rrsettings WHERE messagelink = $1 AND guildid = $2;`,
+    [reactionRow.messagelink, reaction.message.guild.id],
+  );
+
+  if (!res || !res.rowCount) return null;
+  return res.rows[0];
+};
+
+const getRelatedReactions = async (reaction, baseRow, reactionRow) => {
+  const res = await reaction.client.ch.query(
+    `SELECT * FROM rrbuttons WHERE guildid = $1 AND messagelink = $2 AND uniquetimestamp != $3;`,
+    [reaction.message.guild.id, baseRow.messagelink, reactionRow.uniquetimestamp],
+  );
+
+  if (!res || !res.rowCount) return null;
+  return res.rows;
+};
+
+const getHasAnyOfRelated = (relatedReactions, member) => {
+  if (!relatedReactions || !relatedReactions.length) return false;
+
+  let hasAnyOfRelated = false;
+
+  relatedReactions.forEach((row) => {
+    row.roles.forEach((role) => {
+      if (hasAnyOfRelated) return;
+      if (member.roles.cache.has(role)) hasAnyOfRelated = true;
+    });
+  });
+
+  return hasAnyOfRelated;
+};
+
+const takeRoles = async (reaction, reactionRow, baseRow, hasAnyOfRelated, member) => {
+  const rolesToRemove = [];
+
+  reactionRow.roles.forEach((rID) => {
+    if (member.roles.cache.has(rID)) rolesToRemove.push(rID);
+  });
+
+  if (!hasAnyOfRelated && baseRow.anyroles && baseRow.anyroles.length) {
+    baseRow.anyroles.forEach((rID) => {
+      if (member.roles.cache.has(rID)) rolesToRemove.push(rID);
+    });
+  }
+
+  if (rolesToRemove.length) {
+    await member.roles.remove(rolesToRemove).catch(() => {});
+  }
+};
+
+const getReactionRow = async (reaction, emoteIdentifier) => {
+  const res = await reaction.client.ch.query(
+    'SELECT * FROM rrreactions WHERE messagelink = $1 AND emoteid = $2 AND guildid = $3;',
+    [reaction.message.url, emoteIdentifier, reaction.message.guild.id],
+  );
+
+  if (!res || !res.rowCount) return [];
+  return res.rows[0];
 };
