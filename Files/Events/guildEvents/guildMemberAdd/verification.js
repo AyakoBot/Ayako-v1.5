368e7ed3560992e24fc365b4350c77b58d64dde7
Larsundso <Larsundso3@gmail.com>
2022-04-12 18:29:02 +0200

    fixes

--- Files/Events/guildEvents/guildMemberAdd/verification.js
+++ Files/Events/guildEvents/guildMemberAdd/verification.js
@@ -8,17 +8,18 @@ module.exports = {
       member.roles.add(res.rows[0].pendingrole).catch(() => {});
       const msg = {};
       [msg.r] = res.rows;
-      msg.language = await member.client.ch.languageSelector(member.guild);
+      const language = await member.client.ch.languageSelector(member.guild);
       const DM = await user.createDM().catch(() => {});
       if (DM && DM.id && res.rows[0].selfstart) {
         msg.DM = DM;
         [msg.r] = res.rows;
         msg.client = user.client;
-        msg.lan = msg.language.verification;
         msg.user = user;
         msg.member = member;
         msg.guild = member.guild;
-        user.client.slashCommands.get('verify_message_button').startProcess(msg);
+        user.client.slashCommands
+          .get('verify_message_button')
+          .startProcess(msg, null, null, { lan: language.verification, language });
       }
     }
   },
