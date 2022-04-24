d23f0c4cf27ff34324df686ed6b02b8f505e45a5
Larsundso <Larsundso3@gmail.com>
2022-04-24 19:12:37 +0200

    added customization to eris

--- Files/BaseClient/ErisClient.js
+++ Files/BaseClient/ErisClient.js
@@ -1,6 +1,17 @@
 const Eris = require('eris');
 const auth = require('./auth.json');
 
-const eris = new Eris(`Bot ${auth.token}`);
+const eris = new Eris(`Bot ${auth.token}`, {
+  allowedMentions: {
+    everyone: false,
+    roles: false,
+    users: false,
+    repliedUser: false,
+  },
+  defaultImageFormat: 'png',
+  defaultImageSize: 2048,
+  messageLimit: 11885,
+  restMode: true,
+});
 
 module.exports = eris;
