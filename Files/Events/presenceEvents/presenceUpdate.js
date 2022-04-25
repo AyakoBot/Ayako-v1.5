59c88b275fa9d35b5bba1010e64d1f59b47ede4e
Larsundso <Larsundso3@gmail.com>
2022-04-25 23:19:28 +0200

    removed streamroles

--- Files/Events/presenceEvents/presenceUpdate.js
+++ Files/Events/presenceEvents/presenceUpdate.js
@@ -2,6 +2,5 @@ module.exports = {
   async execute(oldPresence, newPresence) {
     require('./game').execute(oldPresence, newPresence);
     require('./status').execute(oldPresence, newPresence);
-    require('./streamroles')(oldPresence, newPresence);
   },
 };
