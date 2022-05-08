ad3e776345c81be774c8b696a14776cb1f2fed67
Larsundso <Larsundso3@gmail.com>
2022-05-08 12:56:19 +0200

    finished disboard

--- Files/Events/readyEvents/disboard.js
+++ Files/Events/readyEvents/disboard.js
@@ -3,59 +3,85 @@ const jobs = require('node-schedule');
 
 module.exports = async () => {
   const client = require('../../BaseClient/DiscordClient');
-  const { ch } = client;
-  const Constants = client.constants;
-  const res = await ch.query('SELECT * FROM disboard;');
+  const res = await client.ch.query('SELECT * FROM disboard WHERE active = true;');
 
-  if (res && res.rowCount > 0) {
-    for (let i = 0; i < res.rows.length; i += 1) {
-      const { lastbump, enabled } = res.rows[i];
-      const guild = client.guilds.cache.get(res.rows[i].guildid);
-      const channel = client.channels.cache.get(res.rows[i].channelid);
-      const timeLeft = lastbump - Date.now();
+  if (!res || !res.rowCount) return;
 
-      if (!enabled) return;
+  for (let i = 0; i < res.rows.length; i += 1) {
+    const guild = client.guilds.cache.get(res.rows[i].guildid);
+    if (!guild) return;
 
-      if (guild && guild.id && channel && channel.id) {
-        if (timeLeft <= 0) end(ch, guild, Constants, channel, res.rows[i]);
-        else {
-          jobs.scheduleJob(new Date(Date.now() + timeLeft), async () => {
-            end(ch, guild, Constants, channel, res.rows[i]);
-          });
-        }
-      }
+    const channel = client.channels.cache.get(res.rows[i].channelid);
+    if (!channel) return;
+
+    if (Number(res.rows[i].nextbump) <= Date.now()) endReminder({ client, guild });
+    else {
+      client.disboardBumpReminders.set(
+        guild.id,
+        jobs.scheduleJob(new Date(Number(res.rows[i].nextbump)), () => {
+          endReminder({ client, guild });
+        }),
+      );
     }
   }
 };
 
-async function end(ch, guild, Constants, channel) {
-  const res = await ch.query('SELECT * FROM disboard WHERE guildid = $1;', [guild.id]);
-  if (res && res.rowCount > 0) {
-    const [row] = res.rows;
-    const language = await ch.languageSelector(guild);
+const endReminder = async (msg) => {
+  const settings = await getSettings(msg);
+  if (!settings) return;
 
-    const embed = new Builders.UnsafeEmbedBuilder()
-      .setDescription(language.ready.disboard.bumpMsg)
-      .setColor(Constants.standard.color)
-      .setTimestamp()
-      .setThumbnail(guild.iconURL())
-      .setAuthor({
-        name: language.ready.disboard.title,
-        iconURL: Constants.standard.image,
-        url: Constants.standard.invite,
-      });
+  const channel =
+    msg.client.channels.cache.get(settings.channelid) ||
+    msg.client.channels.cache.get(settings.tempchannelid);
 
-    ch.send(
-      channel,
-      {
-        embeds: [embed],
-        content: `${row.roles.map((r) => `<@&${r}>`).join(' ')}\n${row.users
-          .map((u) => `<@${u}>`)
-          .join(' ')}`,
+  if (!channel) return;
+
+  const language = await msg.client.ch.languageSelector(msg.guild);
+  const lan = language.ready.disboard;
+
+  const embed = new Builders.UnsafeEmbedBuilder()
+    .setAuthor({
+      name: lan.title,
+      iconURL:
+        'https://cdn.discordapp.com/avatars/302050872383242240/67342a774a9f2d20d62bfc8553bb98e0.webp?size=4096',
+      url: msg.client.constants.standard.invite,
+    })
+    .setDescription(lan.desc)
+    .setColor(msg.client.ch.colorSelector(msg.guild.me));
+
+  const users = settings.users?.map((u) => `<@${u}>`).join(', ') || '';
+  const roles = settings.roles?.map((r) => `<@&${r}>`).join(', ') || '';
+
+  await msg.client.ch.send(channel, { embeds: [embed], content: `${users}\n${roles}` });
+
+  setReminder(msg, false, settings);
+};
+
+const getSettings = async (msg) => {
+  const res = await msg.client.ch.query(
+    'SELECT * FROM disboard WHERE guildid = $1 AND active = true;',
+    [msg.guild.id],
+  );
+
+  if (res && res.rowCount) return res.rows[0];
+  return null;
+};
+
+const setReminder = (msg, isBump, settings) => {
+  if (!isBump && !settings.repeatreminder) return;
+
+  msg.client.ch.query(`UPDATE disboard SET nextbump = $1 WHERE guildid = $2;`, [
+    Date.now() + (isBump ? 7200000 : settings.repeatreminder * 60 * 1000),
+    msg.guild.id,
+  ]);
+
+  msg.client.disboardBumpReminders.set(
+    msg.guild.id,
+    jobs.scheduleJob(
+      new Date(Date.now() + (isBump ? 7200000 : settings.repeatreminder * 60 * 1000)),
+      () => {
+        endReminder(msg);
       },
-      5000,
-    );
-
-    ch.query('UPDATE disboard SET lastbump = NULL WHERE guildid = $1;', [guild.id]);
-  }
-}
+    ),
+  );
+};
