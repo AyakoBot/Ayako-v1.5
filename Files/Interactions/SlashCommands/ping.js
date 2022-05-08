c598359912f8a85ffec178c2532e04764f48976a
Larsundso <Larsundso3@gmail.com>
2022-05-08 12:59:36 +0200

    added ping slash command

---
+++ Files/Interactions/SlashCommands/ping.js
@@ -0,0 +1,14 @@
+module.exports = {
+  name: 'ping',
+  perm: null,
+  dm: true,
+  type: 'info',
+  execute: async (cmd) => {
+    const { heartbeat } = (await cmd.client.ch.query('SELECT heartbeat FROM stats;')).rows[0];
+
+    cmd.reply({
+      content: `🏓 \n**Response Time:** ${cmd.client.ws.ping}ms\n**Last Heartbeat:** ${heartbeat}ms`,
+      ephemeral: true,
+    });
+  },
+};
