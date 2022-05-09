8198fd28a05ed5eff1196c81ef0fa41af4213c52
Larsundso <Larsundso3@gmail.com>
2022-05-10 00:19:56 +0200

    made bypass global

--- Files/Commands/bypass.js
+++ Files/Commands/bypass.js
@@ -1,30 +1,62 @@
+const Builders = require('@discordjs/builders');
+
 module.exports = {
   name: 'bypass',
   perm: 8192n,
   dm: false,
   takesFirstArg: true,
   aliases: null,
-  ThisGuildOnly: ['298954459172700181'],
-  category: 'Moderation',
-  description: 'Let a User skip the Verification',
-  usage: ['bypass [user ID or mention]'],
-  type: 'util',
-  async execute(msg) {
-    const user = msg.args[0].replace(/\D+/g, '');
-    const BypassRole = msg.guild.roles.cache.find((role) => role.id === '389470002992119810');
-    const PreRole = msg.guild.roles.cache.find((role) => role.id === '805315426543599676');
-    const done1 = await msg.member.roles.remove(PreRole).catch(() => {});
-    const done2 = await msg.member.roles.add(BypassRole).catch(() => {});
-    if (!done1 || !done2) {
-      msg.react(msg.client.objectEmotes.cross.id).catch(() => {});
-      msg.client.ch.reply(
-        msg,
-        'Something went wrong when I tried to update the Members Roles. Please check manually.',
-      );
-    } else {
-      const DM = await user.createDM();
-      msg.react(msg.client.objectEmotes.tick.id).catch(() => {});
-      msg.client.ch.send(DM, 'You have been manually bypassed by a Staff Member');
+  type: 'mod',
+  execute: async (msg) => {
+    const member = await msg.guild.members.fetch(msg.args[0].replace(/\D+/g, '')).catch(() => {});
+    if (!member) {
+      msg.client.ch.error(msg, msg.language.errors.memberNotFound);
+      return;
     }
+
+    const settings = await getSettings(msg);
+    if (!settings) {
+      msg.client.ch.error(msg, msg.language.errors.notEnabled);
+      return;
+    }
+
+    const pendingRole = msg.guild.roles.cache.get(settings.pendingrole);
+    const finishedRole = msg.guild.roles.cache.get(settings.finishedrole);
+
+    if (pendingRole && member.roles.cache.has(pendingRole.id)) {
+      member.roles.remove(pendingRole).catch(() => {});
+    }
+
+    if (finishedRole && !member.roles.cache.has(finishedRole.id)){ member.roles.add(finishedRole).catch(() => {});
+}
+    doLog(msg, settings, member);
+
+    msg.client.ch.send(member.user, { content: msg.lan.bypass });
   },
+};
+
+const getSettings = async (msg) => {
+  const res = await msg.client.ch.query(
+    `SELECT * FROM verification WHERE active = true AND guildid = $1;`,
+    [msg.guild.id],
+  );
+
+  if (res && res.rowCount) return res.rows[0];
+  return null;
+};
+
+const doLog = (msg, settings, member) => {
+  const logChannel = msg.guild.channels.cache.get(settings.logchannel);
+  if (!logChannel) return;
+
+  const embed = new Builders.UnsafeEmbedBuilder()
+    .setDescription(msg.client.ch.stp(msg.language.verification?.log?.end, { user: member.user }))
+    .setAuthor({
+      name: member.user.tag,
+      iconURL: member.user.displayAvatarURL({ size: 4096 }),
+    })
+    .setTimestamp()
+    .setColor();
+
+  msg.client.ch.send(logChannel, { embeds: [embed] }, 5000);
 };
