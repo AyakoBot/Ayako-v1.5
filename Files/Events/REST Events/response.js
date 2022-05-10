5ac2a422c576e7ea80ed16149e0d4edd84282412
Larsundso <Larsundso3@gmail.com>
2022-05-10 23:41:46 +0200

    added eris rest log

--- Files/Events/REST Events/response.js
+++ Files/Events/REST Events/response.js
@@ -1,5 +1,6 @@
 const logger = require('../../BaseClient/APILog').resLogger;
 
 module.exports = (request, response) => {
-  logger.log(`${new Date()} | Response |`, response);
+  if (request) logger.log(`${new Date()} | D.js Response |`, response);
+  else logger.log(`${new Date()} | Eris Response |`, response);
 };
