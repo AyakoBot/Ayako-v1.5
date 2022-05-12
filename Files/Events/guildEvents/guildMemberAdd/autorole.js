4f9525c7f99ed76ac56e7b92894b330ecd920dce
Larsundso <Larsundso3@gmail.com>
2022-05-12 19:05:13 +0200

    fixes

--- Files/Events/guildEvents/guildMemberAdd/autorole.js
+++ Files/Events/guildEvents/guildMemberAdd/autorole.js
@@ -1,12 +1,27 @@
 module.exports = async (member) => {
-  const res = await member.client.ch.query('SELECT * FROM autorole WHERE guildid = $1;', [
-    member.guild.id,
-  ]);
-  if (res && res.rowCount > 0) {
-    const roleArray = [];
-    res.rows.forEach((row) => {
-      if (member.guild.roles.cache.get(row.roleid)) roleArray.push(row.roleid);
-    });
-    if (roleArray.length) member.roles.add(roleArray).catch(() => {});
+  const res = await member.client.ch.query(
+    'SELECT * FROM autorole WHERE guildid = $1 AND active = true;',
+    [member.guild.id],
+  );
+  if (!res || !res.rowCount) return;
+
+  let roles = [];
+  const r = res.rows[0];
+
+  if (member.user.bot && r.botroleid?.length) {
+    roles.push(...r.botroleid);
+  } else if (!member.user.bot && r.userroleid?.length) {
+    roles.push(...r.userroleid);
   }
+
+  if (r.allroleid?.length) {
+    roles.push(...r.allroleid);
+  }
+
+  roles = roles.filter(
+    (role) => !member.roles.cache.has(role) && member.guild.roles.cache.get(role),
+  );
+  if (!roles.length) return;
+
+  member.roles.add(roles).catch(() => {});
 };
