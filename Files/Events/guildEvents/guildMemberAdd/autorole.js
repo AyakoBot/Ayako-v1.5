bae4c9266bfbdc7dff951ad8cf921f95960ab845
Larsundso <Larsundso3@gmail.com>
2022-05-12 16:13:36 +0200

    activated autorole.. why was that disabled?

--- Files/Events/guildEvents/guildMemberAdd/autorole.js
+++ Files/Events/guildEvents/guildMemberAdd/autorole.js
@@ -1,16 +1,12 @@
-module.exports = {
-  // eslint-disable-next-line no-unused-vars
-  async execute(member, user) {
-    const { client } = user;
-    const { ch } = client;
-    const { guild } = member;
-    const res = await ch.query('SELECT * FROM autorole WHERE guildid = $1;', [guild.id]);
-    if (res && res.rowCount > 0) {
-      const roleArray = [];
-      res.rows.forEach((row) => {
-        if (guild.roles.cache.get(row.roleid)) roleArray.push(row.roleid);
-      });
-      if (roleArray.length) member.roles.add(roleArray).catch(() => {});
-    }
-  },
+module.exports = async (member) => {
+  const res = await member.client.ch.query('SELECT * FROM autorole WHERE guildid = $1;', [
+    member.guild.id,
+  ]);
+  if (res && res.rowCount > 0) {
+    const roleArray = [];
+    res.rows.forEach((row) => {
+      if (member.guild.roles.cache.get(row.roleid)) roleArray.push(row.roleid);
+    });
+    if (roleArray.length) member.roles.add(roleArray).catch(() => {});
+  }
 };
--- Files/Events/guildEvents/guildMemberAdd/guildMemberAdd.js
+++ Files/Events/guildEvents/guildMemberAdd/guildMemberAdd.js
@@ -4,5 +4,6 @@ module.exports = async (member) => {
   require('./verification')(member);
   require('./antiraid')(member);
   require('./sticky')(member);
+  require('./autorole')(member);
   require('./dmAd')(member);
 };
