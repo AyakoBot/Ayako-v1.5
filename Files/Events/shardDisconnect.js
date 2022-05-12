aa63a243cc610bf5cc6cff6f3947546bd763384b
Larsundso <Larsundso3@gmail.com>
2022-05-12 15:33:14 +0200

    .js end removed

--- Files/Events/shardDisconnect.js
+++ Files/Events/shardDisconnect.js
@@ -2,7 +2,7 @@ const auth = require('../BaseClient/auth.json');
 
 module.exports = {
   async execute(event, id) {
-    const client = require('../BaseClient/DiscordClient.js');
+    const client = require('../BaseClient/DiscordClient');
     client.ch.logger(`Discord Client Shard with ID ${id} was Disconnected!`);
     client.destroy();
     client.login(auth.token);
