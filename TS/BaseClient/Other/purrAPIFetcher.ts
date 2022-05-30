6496afdbabec9529be9987732012ae57d5998a86
Lars_und_so <46791248+Larsundso@users.noreply.github.com>
2022-05-31 00:39:44 +0200

    Revert "restore"
    This reverts commit ebb40c82f06fc8a34ecd35274e1472fb3886d346.

--- TS/BaseClient/Other/purrAPIFetcher.ts
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-export default async (name: string) => {
-  const res = await axios.get(`https://purrbot.site/api/img/sfw/${name}/gif`);
-
-  if (res && res.status === 200) return res.data.link;
-  return null;
-};
