9232daec21896f26251da77502280fd7d0105ab8
Larsundso <Larsundso3@gmail.com>
2022-06-15 01:13:11 +0200

    wrong color :/

--- Files/Events/guildEvents/guildCreate/role.js
+++ Files/Events/guildEvents/guildCreate/role.js
@@ -3,7 +3,7 @@ const jobs = require('node-schedule');
 module.exports = {
   async execute(guild) {
     jobs.scheduleJob(new Date(Date.now() + 600000), () => {
-      const role = guild.roles.cache.find((r) => r.name === guild.client.constants.standard.name);
+      const role = guild.roles.botForRole(guild.client.user);
       if (role && role.id) {
         role.edit({ color: guild.client.constants.standard.color }).catch(() => {});
       }
