7a01a11be5f796ddb066dd2bb65b896bae545df0
Larsundso <Larsundso3@gmail.com>
2022-06-15 22:50:21 +0200

    fix

--- Files/Events/guildEvents/guildCreate/role.js
+++ Files/Events/guildEvents/guildCreate/role.js
@@ -3,7 +3,7 @@ const jobs = require('node-schedule');
 module.exports = {
   async execute(guild) {
     jobs.scheduleJob(new Date(Date.now() + 600000), () => {
-      const role = guild.roles.botForRole(guild.client.user);
+      const role = guild.roles.botRoleFor(guild.client.user);
       if (role && role.id) {
         role.edit({ color: guild.client.constants.standard.color }).catch(() => {});
       }
