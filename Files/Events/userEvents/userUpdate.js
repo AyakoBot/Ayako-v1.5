58a676babf225bd619d31f1067978fb613f4cb17
Larsundso <Larsundso3@gmail.com>
2022-06-22 01:33:53 +0200

    omit bots

--- Files/Events/userEvents/userUpdate.js
+++ Files/Events/userEvents/userUpdate.js
@@ -1,5 +1,6 @@
 module.exports = {
   async execute(oldUser, newUser) {
+    if (oldUser.bot || newUser.bot) return;
     require('./log').execute(oldUser, newUser);
     require('./nameLog').execute(oldUser, newUser);
   },
