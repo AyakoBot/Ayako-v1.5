7501afd25f0cf39d3a283e37979de4c7121d9b8b
Larsundso <Larsundso3@gmail.com>
2022-07-04 11:00:15 +0200

    delete phantom logs

--- Files/Events/guildEvents/guildCreate/guildCreate.js
+++ Files/Events/guildEvents/guildCreate/guildCreate.js
@@ -1,5 +1,7 @@
 module.exports = {
   async execute(guild) {
+    if (!guild.memberCount) return;
+
     require('./log').execute(guild);
     require('./role').execute(guild);
     require('./welcome').execute(guild);
