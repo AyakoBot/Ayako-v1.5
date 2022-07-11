16d7ec65c3646278a5d507cd1f70145016c34be3
Larsundso <Larsundso3@gmail.com>
2022-07-11 20:41:58 +0200

    added voiceChannelJoin log

---
+++ src/Events/voiceChannelEvents/voiceChannelJoin/voiceChannelJoin.ts
@@ -0,0 +1,5 @@
+import type * as Eris from 'eris';
+
+export default async (member: Eris.Member, channel: Eris.TextVoiceChannel | Eris.StageChannel) => {
+  (await import('./log')).default(member, channel);
+};
