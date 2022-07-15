30ec1d87acaac2c5562f92365a443ac0ffc4ec24
Larsundso <Larsundso3@gmail.com>
2022-07-15 23:45:27 +0200

    perm check

--- src/Events/readyEvents/startupTasks/webhooks.ts
+++ src/Events/readyEvents/startupTasks/webhooks.ts
@@ -4,9 +4,15 @@ export default async () => {
   client.guilds.forEach((guild) => {
     guild.channels.forEach(async (channel) => {
       if (!('getWebhooks' in channel)) return;
-      const webhooks = await channel.getWebhooks().catch(() => null);
 
+      const me = await client.ch.getMember(client.user.id, guild.id);
+      if (!me) return;
+
+      if (!channel.permissionsOf(me).has(536870912n)) return;
+
+      const webhooks = await channel.getWebhooks().catch(() => null);
       if (!webhooks) return;
+
       client.webhooks.set(channel.id, webhooks);
     });
   });
