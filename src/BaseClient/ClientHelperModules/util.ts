91126945846f745fede46c4ea3c7e1bca3c2650a
Larsundso <Larsundso3@gmail.com>
2022-07-18 17:51:31 +0200

    added checkVal for command option parsing

--- src/BaseClient/ClientHelperModules/util.ts
+++ src/BaseClient/ClientHelperModules/util.ts
@@ -1,4 +1,11 @@
+import type * as Eris from 'eris';
+
 export const makeCodeBlock = (text: string) => `\`\`\`${text}\`\`\``;
 export const makeInlineCode = (text: string) => `\`${text}\``;
 export const makeBold = (text: string) => `**${text}**`;
 export const makeUnderlined = (text: string) => `__${text}__`;
+export const checkVal = (obj: Eris.InteractionDataOptions | undefined) => {
+  if (!obj) return null;
+  if ('value' in obj) return obj.value;
+  return null;
+};
