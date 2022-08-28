fde71735cb74bc545ad26af73c5193824e8bcf57
Larsundso <Larsundso3@gmail.com>
2022-08-28 10:50:48 +0200

    query manager updates

--- src/BaseClient/ClientHelperModules/query.ts
+++ src/BaseClient/ClientHelperModules/query.ts
@@ -2,16 +2,18 @@ import pool from '../DataBase';
 
 export default async (
   string: string,
-  args?: (string | number | boolean | null)[],
+  args?: (string | number | boolean | null | undefined)[],
   debug?: boolean,
 ) => {
   // eslint-disable-next-line no-console
   if (debug) console.log(string, args);
 
-  const res = await pool.query(string, args).catch((err) => {
-    throw new Error(err);
-  });
+  const res = await pool
+    .query(string, args as (string | number | boolean | null)[])
+    .catch((err) => {
+      throw new Error(err);
+    });
 
   if (!res || !res.length) return null;
-  return res;
+  return res as unknown as never[];
 };
